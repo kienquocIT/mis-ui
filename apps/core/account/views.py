0142a9f5954381ac4b54876206e72795ce643409
phuclh <phuclh@mtsolution.com.vn>
2022-12-21 14:19:26 +0700

    update UI user List + views

--- apps/core/account/views.py
+++ apps/core/account/views.py
@@ -1,19 +1,27 @@
+from django.shortcuts import render, redirect
+
 from rest_framework.views import APIView
 
-from apps.shared import mask_view
+from apps.shared import ServerAPI, ApiURL, mask_view, AuthMsg, ServerMsg
+
+
+TEMPLATE = {
+    'list': 'core/account/user_list.html',
+    'detail': '',
+}
 
 
 class UserListView(APIView):
     @mask_view(auth_require=True, template='core/account/user_list.html')
     def get(self, request, *args, **kwargs):
-        # resp_data = ServerAPI(user=request.user, url=ApiURL.user_list).get()
-        # if resp_data.state:
-        #     result = resp_data.result['result']
-        #     return render(request, template, {})
-        # else:
-        #     raise ValueError(resp_data.errors)
-        user_list = ['abc']
-        return {'user_list': user_list}
+        response_data = ServerAPI(user=request.user, url=ApiURL.user_list).get()
+        if response_data.state:
+            result = response_data.result
+            if result:
+                return render(request, TEMPLATE.get("list"), {'user_list': result})
+        else:
+            raise ValueError(response_data.errors)
+        return {'user_list': result}
 
     @mask_view(auth_require=True, is_api=True)
     def post(self, request, *args, **kwargs):

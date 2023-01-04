6e1a3a70c4ee57b59456d29353b47f33c29c100d
phuclh <phuclh@mtsolution.com.vn>
2023-01-04 09:47:51 +0700

    [ms31] update org list

--- apps/shared/apis/urls_map.py
+++ apps/shared/apis/urls_map.py
@@ -10,6 +10,7 @@ class ApiURL(object):
 
     # organization/group
     GROUP_LIST = 'organization/groups'
+    GROUP_DETAIL = 'organization/group'
 
     # home/company
     COMPANY_LIST = 'tenant/company'

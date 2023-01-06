591f6542d334551db92c372f6cbd834d38744de5
haind <nguyenduonghai07@gmail.com>
2023-01-06 15:42:31 +0700

    clean code

--- apps/core/company/static/core/company/js/load_company_DT.js
+++ apps/core/company/static/core/company/js/load_company_DT.js
@@ -81,7 +81,7 @@ $(function () {
 
                 {
                     'className': 'action-center', 'render': (data, type, row, meta) => {
-                        let bt2 = `<a class="btn btn-icon btn-flush-dark btn-rounded flush-soft-hover" id="edit-company-button" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Edit" href="/tenant/company/`+row.id+`" data-id="`+ row.id +`"><span class="btn-icon-wrap"><span class="feather-icon"><i data-feather="edit"></i></span></span></a>`;
+                        let bt2 = `<a class="btn btn-icon btn-flush-dark btn-rounded flush-soft-hover" id="edit-company-button" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Edit" href="/company/list/`+row.id+`" data-id="`+ row.id +`"><span class="btn-icon-wrap"><span class="feather-icon"><i data-feather="edit"></i></span></span></a>`;
                         let bt3 = `<a class="btn btn-icon btn-flush-dark btn-rounded flush-soft-hover" id="del-company-button" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Delete" href="#" data-id="`+ row.id +`"><span class="btn-icon-wrap"><span class="feather-icon"><i data-feather="trash-2"></i></span></span></a>`;
                         return `<div><center>` + bt2 + bt3 + `</center></div>`;
                     }

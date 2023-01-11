56f185eda449c77349bda31d6953100ca1baf837
namtt <namtt@mtsolution.com.vn>
2023-01-11 13:57:16 +0700

    [feature/ms65] update template role

--- apps/core/hr/static/assets/js/core/hr/role/role_list.js
+++ apps/core/hr/static/assets/js/core/hr/role/role_list.js
@@ -2,8 +2,6 @@
 "use strict";
 $(function () {
     $(document).ready(function () {
-        $("td").addClass("text-center");
-
         let config = {
             dom: '<"row"<"col-7 mb-3"<"blog-toolbar-left">><"col-5 mb-3"<"blog-toolbar-right"flip>>><"row"<"col-sm-12"t>><"row"<"col-sm-12 col-md-5"i><"col-sm-12 col-md-7"p>>',
             ordering: false,

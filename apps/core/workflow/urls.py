82748cfcbe76f6d34646f90ba28474510d290b60
quannm0105 <woongnguyen.15@gmail.com>
2023-02-16 17:13:01 +0700

    [feature/ms-91[workflow list and create] + core workflow day3

--- apps/core/workflow/urls.py
+++ apps/core/workflow/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from apps.core.workflow.views.config import WorkflowList, WorkflowListAPI, WorkflowCreate, WorkflowCreateAPI
+from apps.core.workflow.views.config import WorkflowList, WorkflowListAPI, WorkflowCreate, WorkflowCreateAPI, WorkflowDetail
 
 urlpatterns = [
     path('lists', WorkflowList.as_view(), name='WorkflowList'),
     path('api/lists', WorkflowListAPI.as_view(), name='WorkflowListAPI'),
     path('create', WorkflowCreate.as_view(), name='WorkflowCreate'),
     path('api/create', WorkflowCreateAPI.as_view(), name='WorkflowCreateAPI'),
+    path('detail/<str:pk>', WorkflowDetail.as_view(), name='WorkflowDetail'),
 ]

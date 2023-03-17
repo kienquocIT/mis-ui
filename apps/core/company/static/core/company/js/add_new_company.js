9ef44f21500745d585a9781e01701e82b453ef5d
trieunt <trieunt@mtsolution.com.vn>
2023-03-17 16:01:05 +0700

    [MASTER] Fix notify create company

--- apps/core/company/static/core/company/js/add_new_company.js
+++ apps/core/company/static/core/company/js/add_new_company.js
@@ -4,12 +4,7 @@ $(document).ready(function () {
         let csr = $("input[name=csrfmiddlewaretoken]").val();
         let frm = new SetupFormSubmit($(this));
         $.fn.callAjax(frm.dataUrl, frm.dataMethod, frm.dataForm, csr).then((resp) => {
-            let data = $.fn.switcherResp(resp);
-            if (data) {
-                $.fn.notifyPopup({description: resp.detail}, 'success');
-                $('#AddCompanyForms').modal('hide');
-                setTimeout(location.reload.bind(location), 1000);
-            }
+            return $.fn.switcherResp(resp);
         }, (err) => {
             $.fn.notifyPopup({description: err.detail}, 'failure');
         })

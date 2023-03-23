a67f900e398d6384e0e64f5d166eb52bfd8b8c36
namtt <namtt@mtsolution.com.vn>
2023-03-23 11:26:33 +0700

    [feature/ms-153] update js page create product

--- apps/sale/saledata/static/assets/js/sale/saledata/product/product_create.js
+++ apps/sale/saledata/static/assets/js/sale/saledata/product/product_create.js
@@ -5,9 +5,11 @@ $(document).ready(function () {
         if (!check) {
             $(link_tab).addClass('disabled');
             $(id_tab).removeClass('active show');
-            $('#tab_general').addClass('active show');
-            $('a[href="#tab_general"]').addClass('active');
-            $(`a[href="`+id_tab+`"]`).removeClass('active');
+            if ($(`a[href="` + id_tab + `"]`).hasClass('active')) {
+                $('a[href="#tab_general"]').addClass('active');
+                $('#tab_general').addClass('active show');
+            }
+            $(`a[href="` + id_tab + `"]`).removeClass('active');
         } else {
             $(link_tab).removeClass('disabled');
         }
@@ -61,7 +63,7 @@ $(document).ready(function () {
         },)
     }
 
-    function loadUoMGroup(){
+    function loadUoMGroup() {
         let ele = $('#select-box-umo-group');
         ele.html('');
         $.fn.callAjax(ele.attr('data-url'), ele.attr('data-method')).then((resp) => {
@@ -78,38 +80,41 @@ $(document).ready(function () {
         },)
     }
 
-    function loadUnitOfMeasure(){
+    loadProductCategory();
+    loadProductType();
+    loadUoMGroup();
+
+    // change select box UoM group tab general
+    $('#select-box-umo-group').on('change', function (){
+        let data_url = $(this).attr('data-url-detail').replace(0,$(this).val());
+        let data_method = $(this).attr('data-method');
         let select_box_default_uom = $('#select-box-default-uom');
-        let select_box_uom_code = $('#select-box-uom-code');
         let select_box_uom_name = $('#select-box-uom-name');
 
-        $.fn.callAjax(select_box_default_uom.attr('data-url'), select_box_default_uom.attr('data-method')).then((resp) => {
+        $.fn.callAjax(data_url, data_method).then((resp) => {
             let data = $.fn.switcherResp(resp);
             if (data) {
-                if (resp.hasOwnProperty('data') && resp.data.hasOwnProperty('unit_of_measure')) {
+                if (resp.hasOwnProperty('data') && resp.data.hasOwnProperty('uom_group')) {
                     select_box_default_uom.append(`<option value="null"></option>`);
-                    select_box_uom_name.append(`<option value="null"></option>`);
-                    select_box_uom_code.append(`<option value="null"></option>`);
-                    resp.data.unit_of_measure.map(function (item) {
-                        select_box_default_uom.append(`<option value="` + item.id + `">` + item.title + `</option>`);
-                        select_box_uom_name.append(`<option value="` + item.id + `">` + item.title + `</option>`);
-                        select_box_uom_code.append(`<option value="` + item.id + `">` + item.code + `</option>`);
+                    select_box_uom_name.append(`<option value="null" data-code=""></option>`);
+                    data.uom_group.uom.map(function (item) {
+                        select_box_default_uom.append(`<option value="` + item.uom_id + `">` + item.uom_title + `</option>`);
+                        select_box_uom_name.append(`<option value="` + item.uom_id + `" data-code="` + item.uom_code + `">` + item.uom_title + `</option>`);
                     })
                 }
             }
         }, (errs) => {
         },)
-    }
+    })
 
-    loadProductCategory();
-    loadProductType();
-    loadUoMGroup();
-    loadUnitOfMeasure();
-
+    // change select box UoM Name in tab inventory
+    $('#select-box-uom-name').on('change', function (){
+        $('#uom-code').val($(this).find(":selected").attr('data-code'));
+    })
 
     //submit form create product
     let form_create_product = $('#form-create-product');
-    form_create_product.submit(function (event){
+    form_create_product.submit(function (event) {
         event.preventDefault();
         let csr = $("input[name=csrfmiddlewaretoken]").val();
         let frm = new SetupFormSubmit($(this));

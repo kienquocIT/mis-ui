f29f1d31e30883ee39fab1eac14c9ec31845af1e
quannm <quannm@mtsolution.com.vn>
2023-03-31 16:41:43 +0700

    [feature/ms-91][workflow detail] + additional UI node effect, fix bug workflow-chart.js

--- apps/core/workflow/static/assets/core/workflow/js/wf_detail.js
+++ apps/core/workflow/static/assets/core/workflow/js/wf_detail.js
@@ -12,10 +12,12 @@ $(function () {
             initSelectbox(elmApp)
         }
         if (res.is_define_zone) $('[name="define_zone"]').val(res.is_define_zone);
-        if (res.zone) initTableZone(res.zone);
-        if (res.zone) $('#zone-list').val(JSON.stringify(res.zone));
+        if (res.zone){
+            initTableZone(res.zone);
+            $('#zone-list').val(JSON.stringify(res.zone));
+        }
         if (res.node) $('#node-list').val(JSON.stringify(res.node));
-        if (res.association) $('#associate-connect').val(JSON.stringify(res.association))
+        if (res.association) $('#node-associate').val(JSON.stringify(res.association))
     }
 
     /***

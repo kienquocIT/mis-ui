51b41d897c059442536e7e4cb0b72a2ef4dd530a
phuclh <phuclh@mtsolution.com.vn>
2023-04-10 11:31:31 +0700

    [feature/ms-223] add quotation_create.css

---
+++ apps/sales/quotation/static/assets/sales/quotation/js/quotation_create.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(function () {
+
+    $(document).ready(function () {
+
+        $("#select-box-quotation-term-price").select2();
+        $("#select-box-quotation-term-discount").select2();
+
+    });
+});

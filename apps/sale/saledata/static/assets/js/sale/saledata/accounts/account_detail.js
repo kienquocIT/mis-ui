8ff79867c40dd64dbdbeb15b65f893b6991abcc1
haind <nguyenduonghai07@gmail.com>
2023-04-22 12:15:44 +0700

    [feature/ms-150] Account Detail Page

--- apps/sale/saledata/static/assets/js/sale/saledata/accounts/account_detail.js
+++ apps/sale/saledata/static/assets/js/sale/saledata/accounts/account_detail.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
 
+    // load data detail
+    let pk = window.location.pathname.split('/').pop();
+    let url_loaded = $('#form-detail-account').attr('data-url').replace(0, pk);
+    $.fn.callAjax(url_loaded, 'GET').then(
+        (resp) => {
+            let data = $.fn.switcherResp(resp);
+            if (data) {
+                data = data['account_detail'];
+                console.log(data);
+                $('#account-title-id').val(data.name);
+                $('#account-code-id').val(data.code);
+                $('#account-website-id').val(data.website);
+                $('#account-owner-id').val(data.owner[0].fullname);
+            }
+        })
+
 })

ffbcbe1c5aac9cb1046d71f98bb526f34772b1b4
phuclh <phuclh@mtsolution.com.vn>
2023-04-23 23:24:29 +0700

    [feature/ms-223] Viết js tạo opportunity

--- apps/sales/opportunity/static/assets/sales/opportunity/js/opportunity_list.js
+++ apps/sales/opportunity/static/assets/sales/opportunity/js/opportunity_list.js
@@ -1,6 +1,8 @@
 "use strict";
 $(function () {
     $(document).ready(function () {
+        let boxCustomer = $('#select-box-opportunity-create-customer');
+
         let $table = $('#table_opportunity_list')
         let listURL = $table.attr('data-url')
         let _dataTable = $table.DataTable({
@@ -82,6 +84,63 @@ $(function () {
             const keycode = evt.which;
             if (keycode === 13) //enter to search
                 _dataTable.ajax.reload()
-        })
+        });
+
+        // Action on click dropdown customer
+        boxCustomer.on('click', function (e) {
+            if (!$(this)[0].innerHTML) {
+                let url = $(this).attr('data-url');
+                let method = $(this).attr('data-method');
+                $.fn.callAjax(url, method).then(
+                    (resp) => {
+                        let data = $.fn.switcherResp(resp);
+                        if (data) {
+                            if (data.hasOwnProperty('account_list') && Array.isArray(data.account_list)) {
+                                boxCustomer.append(`<option value=""></option>`);
+                                data.account_list.map(function (item) {
+                                    boxCustomer.append(`<option value="${item.id}">
+                                                            <span class="account-title">${item.name}</span>
+                                                        </option>`)
+                                })
+                            }
+                        }
+                    }
+                )
+            }
+        });
+
+        $('#btn-create_opportunity').on('click', function (e) {
+            e.preventDefault()
+            let $form = document.getElementById('form-create_opportunity');
+            let _form = new SetupFormSubmit($('#form-create_opportunity'));
+            let submitFields = [
+                'title',
+                'code',
+                'customer'
+            ]
+            if (_form.dataForm) {
+                for (let key in _form.dataForm) {
+                    if (!submitFields.includes(key)) delete _form.dataForm[key]
+                }
+            }
+            let csr = $("[name=csrfmiddlewaretoken]").val();
+            $.fn.callAjax(_form.dataUrl, _form.dataMethod, _form.dataForm, csr)
+                .then(
+                    (resp) => {
+                        let data = $.fn.switcherResp(resp);
+                        if (data) {
+                            $.fn.notifyPopup({description: data.message}, 'success')
+                            $.fn.redirectUrl($($form).attr('data-url-redirect'), 3000);
+                        }
+                    },
+                    (errs) => {
+                        console.log(errs)
+                        $.fn.notifyPopup({description: "Opportunity create fail"}, 'failure')
+                    }
+                )
+        });
+
+
+
     });
 });
--- apps/sales/opportunity/views.py
+++ apps/sales/opportunity/views.py
@@ -4,7 +4,17 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from django.utils.translation import gettext_lazy as _
 
-from apps.shared import mask_view, ServerAPI, ApiURL, WorkflowMsg, ConditionFormset
+from apps.shared import mask_view, ServerAPI, ApiURL, WorkflowMsg, ConditionFormset, SaleMsg
+
+
+def create_update_opportunity(request, url, msg):
+    resp = ServerAPI(user=request.user, url=url).post(request.data)
+    if resp.state:
+        resp.result['message'] = msg
+        return resp.result, status.HTTP_201_CREATED
+    elif resp.status == 401:
+        return {}, status.HTTP_401_UNAUTHORIZED
+    return {'errors': resp.errors}, status.HTTP_400_BAD_REQUEST
 
 
 class OpportunityList(View):
@@ -33,3 +43,14 @@ class OpportunityListAPI(APIView):
         elif resp.status == 401:
             return {}, status.HTTP_401_UNAUTHORIZED
         return {'errors': _('Failed to load resource')}, status.HTTP_400_BAD_REQUEST
+
+    @mask_view(
+        auth_require=True,
+        is_api=True
+    )
+    def post(self, request, *args, **kwargs):
+        return create_update_opportunity(
+            request=request,
+            url=ApiURL.OPPORTUNITY_LIST,
+            msg=SaleMsg.OPPORTUNITY_CREATE
+        )

ebf8ef06c67aa3ab184a9d8888546a685cd9427e
haind <nguyenduonghai07@gmail.com>
2023-05-12 17:12:42 +0700

    [feature/ms-287] update UI Accoutn Detail

--- apps/masterdata/saledata/static/assets/js/masterdata/saledata/accounts/account_list.js
+++ apps/masterdata/saledata/static/assets/js/masterdata/saledata/accounts/account_list.js
@@ -160,6 +160,9 @@ $(document).ready(function () {
                     }
                 }, {
                     'className': 'action-center', 'render': (data, type, row, meta) => {
+                        // let bt2 = `<a class="btn btn-icon btn-flush-dark btn-rounded flush-soft-hover edit-button" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Edit" href=""><span class="btn-icon-wrap"><span class="feather-icon"><i data-feather="edit"></i></span></span></a>`;
+                        // let bt3 = `<a class="btn btn-icon btn-flush-dark btn-rounded flush-soft-hover del-button" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Delete" href="#"><span class="btn-icon-wrap"><span class="feather-icon"><i data-feather="trash-2"></i></span></span></a>`;
+                        // return bt2 + bt3;
                     }
                 },]
             }

feee55d6773a8c2a142f18d8b9a6fa9846dfe272
quannm <quannm@mtsolution.com.vn>
2023-05-25 14:25:41 +0700

    [dark-mode][dark mode UI] + release dark mode ver 1.0.1

--- statics/assets/js/dark-mode.js
+++ statics/assets/js/dark-mode.js
@@ -1,3 +1,18 @@
+// set default mode
+(function () {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        const savedTheme = localStorage.getItem("theme") || "light";
+        if (savedTheme){
+            document.body.setAttribute("data-theme", "dark")
+            document.body.classList.add('theme-dark')
+            document.querySelector('.hk-wrapper').setAttribute('data-menu', 'dark')
+        }
+    } else {
+        document.body.removeAttribute("data-theme")
+        document.body.classList.add('theme-light')
+    }
+})();
+
 function applyTheme(theme) {
     document.body.classList.remove("theme-light", "theme-dark");
     document.body.classList.add(`theme-${theme}`);
@@ -5,6 +20,8 @@ function applyTheme(theme) {
     document.querySelector('#dark_mode_btn').checked = theme === 'dark'
 }
 
+
+// run btn dark mode when doc ready
 document.addEventListener("DOMContentLoaded", () => {
     const savedTheme = localStorage.getItem("theme") || "light";
     applyTheme(savedTheme);

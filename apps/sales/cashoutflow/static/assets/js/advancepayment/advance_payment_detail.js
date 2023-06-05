ff741296718b6915b1449688acd077bfd9d08f6c
haind <nguyenduonghai07@gmail.com>
2023-06-05 18:29:13 +0700

    [feature/ms-339] update AP detail page

--- apps/sales/cashoutflow/static/assets/js/advancepayment/advance_payment_detail.js
+++ apps/sales/cashoutflow/static/assets/js/advancepayment/advance_payment_detail.js
@@ -209,7 +209,7 @@ $(document).ready(function () {
                 maxYear: parseInt(moment().format('YYYY')) + 100
             });
 
-            loadAdvanceList(advance_payment.converted_payment_list, advance_payment.return_value);
+            loadAdvanceList(advance_payment.converted_payment_list, advance_payment.return_value, advance_payment.advance_value);
         }
 
         $('.form-control').prop('disabled', true);
@@ -296,7 +296,7 @@ $(document).ready(function () {
         })
     });
 
-    function loadAdvanceList(converted_payment_list, total_return_value) {
+    function loadAdvanceList(converted_payment_list, total_return_value, total_AP_value) {
         if (!$.fn.DataTable.isDataTable('#datatable_payment_list')) {
             let dtb = $('#datatable_payment_list');
             dtb.DataTableDefault({
@@ -340,9 +340,11 @@ $(document).ready(function () {
                     let remain_ap_value = total_ap_value - total_payment_value - total_return_value;
 
                     // Update footer
-                    $(api.column(2).footer()).html(`<br><p class="text-primary"><b>` + total_payment_value.toLocaleString('en-US').replace(/,/g, '.') + ` VNĐ<br>
+                    $(api.column(2).footer()).html(`<p class="text-primary"><b>
+                                                         <br>` + total_AP_value.toLocaleString('en-US').replace(/,/g, '.') + ` VNĐ<br>
+                                                         <br>` + total_payment_value.toLocaleString('en-US').replace(/,/g, '.') + ` VNĐ<br>
                                                          <br>` + total_return_value.toLocaleString('en-US').replace(/,/g, '.') + ` VNĐ<br>
-                                                         <br>` + remain_ap_value.toLocaleString('en-US').replace(/,/g, '.') + ` VNĐ</b></p><br>`);
+                                                         <br>` + remain_ap_value.toLocaleString('en-US').replace(/,/g, '.') + ` VNĐ<br></b></p>`);
                 },
             });
 

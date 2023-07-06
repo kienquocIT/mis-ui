cc8715c16b8e6b1f1be9c80860c15151ab80ea84
haind <nguyenduonghai07@gmail.com>
2023-07-06 16:47:27 +0700

    [feature/ms-360] map call log to Opportunity Detail Page

--- apps/sales/opportunity/static/assets/sales/opportunity/js/opportunity_detail.js
+++ apps/sales/opportunity/static/assets/sales/opportunity/js/opportunity_detail.js
@@ -1457,4 +1457,109 @@ $(document).ready(function () {
     } else {
         setTimeout(autoLoadStage, 1500);
     }
+
+
+
+
+    // for calllog
+    function LoadSaleCodeList() {
+        let $sale_code_sb = $('#sale-code-select-box');
+        $.fn.callAjax($sale_code_sb.attr('data-url'), $sale_code_sb.attr('data-method')).then((resp) => {
+            let data = $.fn.switcherResp(resp);
+            if (data) {
+                if (resp.hasOwnProperty('data') && resp.data.hasOwnProperty('opportunity_list')) {
+                    $sale_code_sb.append(`<option></option>`)
+                    data.opportunity_list.map(function (item) {
+                        $sale_code_sb.append(`<option value="${item.id}">(${item.code})&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${item.title}</option>`);
+                    })
+                }
+            }
+        })
+
+        $sale_code_sb.select2();
+    }
+    function LoadCustomerList() {
+        let $account_sb = $('#account-select-box');
+        $.fn.callAjax($account_sb.attr('data-url'), $account_sb.attr('data-method')).then((resp) => {
+            let data = $.fn.switcherResp(resp);
+            if (data) {
+                if (resp.hasOwnProperty('data') && resp.data.hasOwnProperty('account_list')) {
+                    $account_sb.append(`<option></option>`)
+                    data.account_list.map(function (item) {
+                        $account_sb.append(`<option value="${item.id}">${item.name}</option>`);
+                    })
+                }
+            }
+        })
+
+        $account_sb.select2();
+    }
+    function LoadContactList() {
+        let $contact_sb = $('#contact-select-box');
+        $.fn.callAjax($contact_sb.attr('data-url'), $contact_sb.attr('data-method')).then((resp) => {
+            let data = $.fn.switcherResp(resp);
+            if (data) {
+                if (resp.hasOwnProperty('data') && resp.data.hasOwnProperty('contact_list')) {
+                    $contact_sb.append(`<option></option>`)
+                    data.contact_list.map(function (item) {
+                        $contact_sb.append(`<option value="${item.id}">${item.fullname}</option>`);
+                    })
+                }
+            }
+        })
+
+        $contact_sb.select2();
+    }
+    LoadSaleCodeList();
+    LoadCustomerList();
+    LoadContactList();
+
+    $('#date-input').daterangepicker({
+        singleDatePicker: true,
+        timePicker: true,
+        showDropdowns: true,
+        drops: 'auto',
+        minYear: parseInt(moment().format('YYYY-MM-DD'), 10) - 1,
+        locale: {
+            format: 'YYYY-MM-DD hh:mm:ss'
+        },
+        "cancelClass": "btn-secondary",
+        maxYear: parseInt(moment().format('YYYY'), 10) + 100
+    });
+
+    $('#form-create-new-call-log').submit(function (event) {
+        event.preventDefault();
+        let csr = $("input[name=csrfmiddlewaretoken]").val();
+        let frm = new SetupFormSubmit($(this));
+
+        frm.dataForm['subject'] = $('#subject-input').val();
+        frm.dataForm['opportunity'] = $('#sale-code-select-box').val();
+        frm.dataForm['customer'] = $('#account-select-box').val();
+        frm.dataForm['contact'] = $('#contact-select-box').val();
+        frm.dataForm['call_date'] = $('#date-input').val();
+        frm.dataForm['result'] = $('#result-text-area').val();
+        if ($('#repeat-activity').is(':checked')) {
+            frm.dataForm['repeat'] = 1;
+        }
+        else {
+            frm.dataForm['repeat'] = 0;
+        }
+
+        $.fn.callAjax(frm.dataUrl, frm.dataMethod, frm.dataForm, csr)
+        .then(
+            (resp) => {
+                let data = $.fn.switcherResp(resp);
+                if (data) {
+                    $.fn.notifyPopup({description: "Successfully"}, 'success')
+                    $('#create-new-call-log').hide();
+                    $('#subject-input').val('');
+                    $('#result-text-area').val('');
+                    $('#repeat-activity').prop('checked', false);
+                }
+            },
+            (errs) => {
+                // $.fn.notifyPopup({description: errs.data.errors}, 'failure');
+            }
+        )
+    })
 })

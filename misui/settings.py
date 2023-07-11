c81a7ca7b09f3a264a29c995397d6853fc5e7de0
trieunt <trieunt@mtsolution.com.vn>
2023-07-11 11:07:03 +0700

    [SIT] Enable clear compressor CACHE when build.

--- misui/settings.py
+++ misui/settings.py
@@ -348,6 +348,7 @@ if OS_DEBUG is True or OS_DEBUG in [1, '1']:
     print(Fore.BLUE, f'#  1. DATABASES: {str(DATABASES)} \033[0m')
     print(Fore.YELLOW, f'#  2. API_DOMAIN: {str(API_DOMAIN)} \033[0m')
     print(Fore.LIGHTBLUE_EX, f'#  3. TRACING [JAEGER]: {JAEGER_TRACING_ENABLE}')
+    print(Fore.GREEN, f'#  4. COMPRESSOR ENABLE: {COMPRESS_ENABLED}')
     print(Fore.CYAN, '----------------------------------------------------------------------------------', '\033[0m')
 else:
     DEBUG = False

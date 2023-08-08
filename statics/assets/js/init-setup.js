3a45cd29165f5906cdb5211f3f57aa10b9ab60d5
trieunt <trieunt@mtsolution.com.vn>
2023-08-08 09:59:49 +0700

    [SIT] Make sure ID in permission_by_configured when render page.

--- statics/assets/js/init-setup.js
+++ statics/assets/js/init-setup.js
@@ -1551,6 +1551,14 @@ class UtilControl {
         return result;
     }
 
+    static generateUUID4() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            let r = Math.random() * 16 | 0,
+                v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
     static arraysEqual(a, b) {
         if (a.length !== b.length) return false;
         return a.every((value, index) => value === b[index]);

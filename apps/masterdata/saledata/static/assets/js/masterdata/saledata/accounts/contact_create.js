1febc6ff7e21e14e2317f4c49f8351c44d16c2fc
trieunt <trieunt@mtsolution.com.vn>
2023-08-22 15:25:19 +0700

    [SIT] Fix Contact create: Convert to validate form

--- apps/masterdata/saledata/static/assets/js/masterdata/saledata/accounts/contact_create.js
+++ apps/masterdata/saledata/static/assets/js/masterdata/saledata/accounts/contact_create.js
@@ -1,26 +1,28 @@
 $(document).ready(function () {
     new ContactHandle().load();
 
-    const frmCreate = $('#form-create-contact')
-    frmCreate.submit(function (event) {
-        event.preventDefault();
-        WindowControl.showLoading();
-        let combinesData = new ContactHandle().combinesData($(this));
-        $.fn.callAjax2(combinesData).then(
-            (resp) => {
-                let data = $.fn.switcherResp(resp);
-                if (data && (data['status'] === 201 || data['status'] === 200)) {
-                    $.fn.notifyB({description: $('#base-trans-factory').attr('data-success')}, 'success');
+    const frm = new SetupFormSubmit($('#form-create-contact'));
+    frm.validate({
+        submitHandler: function (event) {
+            event.preventDefault();
+            WindowControl.showLoading();
+            let combinesData = new ContactHandle().combinesData($(this));
+            $.fn.callAjax2(combinesData).then(
+                (resp) => {
+                    let data = $.fn.switcherResp(resp);
+                    if (data && (data['status'] === 201 || data['status'] === 200)) {
+                        $.fn.notifyB({description: $('#base-trans-factory').attr('data-success')}, 'success');
+                        setTimeout(() => {
+                            window.location.replace($(this).attr('data-url-redirect'));
+                            location.reload.bind(location);
+                        }, 1000);
+                    }
+                }, (err) => {
                     setTimeout(() => {
-                        window.location.replace($(this).attr('data-url-redirect'));
-                        location.reload.bind(location);
-                    }, 1000);
+                        WindowControl.hideLoading();
+                    }, 1000)
                 }
-            }, (err) => {
-                setTimeout(() => {
-                    WindowControl.hideLoading();
-                }, 1000)
-            }
-        )
+            )
+        }
     })
 })

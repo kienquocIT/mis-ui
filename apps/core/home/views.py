8c7d8ca07d97154527e9220f3669e95cb1690bcb
trieunt <trieunt@mtsolution.com.vn>
2023-09-29 18:24:21 +0700

    [SIT] Translate fix + move component to utilities

--- apps/core/home/views.py
+++ apps/core/home/views.py
@@ -111,12 +111,57 @@ class DocPinedDetailAPI(APIView):
 class ComponentCollections(View):
     @mask_view(
         auth_require=True,
-        template='core/home/components.html',
+        template='core/utilities/components.html',
         breadcrumb='COMPONENTS_PAGE'
     )
     def get(self, request, *args, **kwargs):
         cond_formset = ConditionFormset()
         return {"form": cond_formset}, status.HTTP_200_OK
+
+
+class UtilitiesView(View):
+    @mask_view(
+        auth_require=True,
+        template='core/utilities/index.html',
+    )
+    def get(self, request, *args, **kwargs):
+        return {}, status.HTTP_200_OK
+
+
+class DefaultDataView(View):
+    @classmethod
+    def callback_success(cls, result):
+        rs = []
+        for plan, plan_data in result.items():
+            for feature, feature_data in plan_data.items():
+                row = []
+                data_tmp = []
+                for data in feature_data:
+                    row_tmp = []
+                    arr_tmp = []
+                    for key in data.keys():
+                        row_tmp.append(key)
+                        arr_tmp.append(data[key])
+                    if not row:
+                        row = row_tmp
+                    data_tmp.append(arr_tmp)
+                rs.append(
+                    {
+                        'plan': plan,
+                        'feature': feature,
+                        'rows': row,
+                        'data': data_tmp,
+                    }
+                )
+        return {'default_data': rs}
+
+    @mask_view(
+        auth_require=True,
+        template='core/utilities/default_data.html',
+    )
+    def get(self, request, *args, **kwargs):
+        resp = ServerAPI(request=request, user=request.user, url='private-system/default-data').get()
+        return resp.auto_return(callback_success=self.callback_success)
 
 
 class TermsAndConditionsView(View):
@@ -132,15 +177,6 @@ class HelpAndSupportView(View):
     @mask_view(
         auth_require=True,
         template='core/help_support/help_support.html',
-    )
-    def get(self, request, *args, **kwargs):
-        return {}, status.HTTP_200_OK
-
-
-class UtilitiesView(View):
-    @mask_view(
-        auth_require=True,
-        template='core/utilities/index.html',
     )
     def get(self, request, *args, **kwargs):
         return {}, status.HTTP_200_OK

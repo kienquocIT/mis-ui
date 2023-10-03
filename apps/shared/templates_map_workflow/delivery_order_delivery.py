37b413d8d7eba706f2aa503a9894d7869773c544
quannm <quannm@mtsolution.com.vn>
2023-10-03 10:32:54 +0700

    [feature/ms-303][picking delivery] + add master data of workflow

--- apps/shared/templates_map_workflow/delivery_order_delivery.py
+++ apps/shared/templates_map_workflow/delivery_order_delivery.py
@@ -10,4 +10,19 @@ DELIVERY_DATA_MAP = {
     'remarks': FieldMapCommon(
         name_mapping=['remarks'],
     ).data,
+    'employee_inherit': FieldMapCommon(
+        name_mapping=['employee_inherit'],
+    ).data,
+    'attachments': FieldMapCommon(
+        name_mapping=['attachments'],
+    ).data,
+    'shipping_address': FieldMapCommon(
+        name_mapping=['shipping_address'],
+    ).data,
+    'billing_address': FieldMapCommon(
+        name_mapping=['billing_address'],
+    ).data,
+    'ready_quantity': FieldMapCommon(
+        name_mapping=['ready_quantity'],
+    ).data,
 }

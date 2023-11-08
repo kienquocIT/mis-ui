5ebd5222a659ede0c62305ee7985b1cb0f091399
phuclh <phuclh@mtsolution.com.vn>
2023-11-08 18:02:56 +0700

    [feature/ms-274] them html & js cho report product, report customer

---
+++ apps/sales/report/static/assets/sales/report/js/report_customer.js
@@ -0,0 +1,178 @@
+$(function () {
+    $(document).ready(function () {
+
+        let boxGroup = $('#box-report-customer-group');
+        let boxEmployee = $('#box-report-customer-employee');
+        let boxCustomer = $('#box-report-customer-product');
+        let btnView = $('#btn-view');
+        let eleRevenue = $('#report-customer-revenue');
+        let eleGrossProfit = $('#report-customer-gross-profit');
+        let eleNetIncome = $('#report-customer-net-income');
+        let $table = $('#table_report_customer_list');
+
+        function loadDbl(data) {
+            $table.DataTableDefault({
+                data: data ? data : [],
+                columns: [
+                    {
+                        targets: 0,
+                        class: 'w-5',
+                        render: (data, type, row) => {
+                            return `<p>${row?.['customer']?.['code'] ? row?.['customer']?.['code'] : ''}</p>`;
+                        }
+                    },
+                    {
+                        targets: 1,
+                        class: 'w-20',
+                        render: (data, type, row) => {
+                            return `<p>${row?.['customer']?.['title'] ? row?.['customer']?.['title'] : ''}</p>`;
+                        }
+                    },
+                    {
+                        targets: 2,
+                        class: 'w-15',
+                        render: (data, type, row) => {
+                            return `<p>${row?.['customer']?.['industry']?.['title'] ? row?.['customer']?.['industry']?.['title'] : ''}</p>`;
+                        }
+                    },
+                    {
+                        targets: 3,
+                        class: 'w-20',
+                        render: (data, type, row) => {
+                            return `<span class="mask-money table-row-revenue" data-init-money="${parseFloat(row?.['revenue'])}"></span>`;
+                        }
+                    },
+                    {
+                        targets: 4,
+                        class: 'w-20',
+                        render: (data, type, row) => {
+                            return `<span class="mask-money table-row-gross-profit" data-init-money="${parseFloat(row?.['gross_profit'])}"></span>`;
+                        }
+                    },
+                    {
+                        targets: 5,
+                        class: 'w-20',
+                        render: (data, type, row) => {
+                            return `<span class="mask-money table-row-net-income" data-init-money="${parseFloat(row?.['net_income'])}"></span>`;
+                        }
+                    },
+                ],
+                drawCallback: function () {
+                    // mask money
+                    $.fn.initMaskMoney2();
+                },
+            });
+        }
+
+        function loadBoxEmployee() {
+            if (boxGroup.val()) {
+                boxEmployee.initSelect2({
+                    'dataParams': {'group_id': boxGroup.val()},
+                    callbackTextDisplay: function (item) {
+                        return item?.['full_name'] || '';
+                    },
+                });
+            } else {
+                boxEmployee.initSelect2({
+                    'allowClear': true,
+                    callbackTextDisplay: function (item) {
+                        return item?.['full_name'] || '';
+                    },
+                });
+            }
+        }
+
+        function loadTotal() {
+            let newRevenue = 0;
+            let newGrossProfit = 0;
+            let newNetIncome = 0;
+            $table.DataTable().rows().every(function () {
+                let row = this.node();
+                let rowRevenue = row?.querySelector('.table-row-revenue')?.getAttribute('data-init-money');
+                let rowGrossProfit = row?.querySelector('.table-row-gross-profit')?.getAttribute('data-init-money');
+                let rowNetIncome = row?.querySelector('.table-row-net-income')?.getAttribute('data-init-money');
+                if (rowRevenue) {
+                    newRevenue += parseFloat(rowRevenue);
+                }
+                if (rowGrossProfit) {
+                    newGrossProfit += parseFloat(rowGrossProfit);
+                }
+                if (rowNetIncome) {
+                    newNetIncome += parseFloat(rowNetIncome);
+                }
+            });
+            eleRevenue.attr('data-init-money', String(newRevenue));
+            eleGrossProfit.attr('data-init-money', String(newGrossProfit));
+            eleNetIncome.attr('data-init-money', String(newNetIncome));
+        }
+
+        loadDbl();
+
+        boxGroup.initSelect2({'allowClear': true,});
+        loadBoxEmployee();
+
+        // run datetimepicker
+        $('input[type=text].date-picker').daterangepicker({
+            minYear: 1901,
+            timePicker: true,
+            showDropdowns: true,
+            locale: {
+                format: 'DD/MM/YYYY'
+            }
+        });
+
+        // mask money
+        $.fn.initMaskMoney2();
+
+        // Events
+        boxGroup.on('change', function() {
+            boxEmployee.empty();
+            loadBoxEmployee();
+            $table.DataTable().clear().draw();
+            loadTotal();
+        });
+
+        boxEmployee.on('change', function() {
+            $table.DataTable().clear().draw();
+            loadTotal();
+        });
+
+        btnView.on('click', function () {
+            let dataParams = {};
+            if (boxGroup.val()) {
+                dataParams['group_inherit_id'] = boxGroup.val();
+            }
+            if (boxEmployee.val()) {
+                dataParams['employee_inherit_id'] = boxEmployee.val();
+            }
+            let date = $('#report-customer-date-approved').val();
+            if (date) {
+                let dateStrings = date.split(' - ');
+                let dateStart = dateStrings[0] + " 00:00:00";
+                let dateEnd = dateStrings[1] + " 23:59:59";
+                dataParams['date_approved__gte'] = moment(dateStart, 'DD/MM/YYYY hh:mm:ss').format('YYYY-MM-DD hh:mm:ss');
+                dataParams['date_approved__lte'] = moment(dateEnd, 'DD/MM/YYYY hh:mm:ss').format('YYYY-MM-DD hh:mm:ss');
+            }
+            $.fn.callAjax2({
+                    'url': $table.attr('data-url'),
+                    'method': $table.attr('data-method'),
+                    'data': dataParams,
+                    // 'isDropdown': true,
+                }
+            ).then(
+                (resp) => {
+                    let data = $.fn.switcherResp(resp);
+                    if (data) {
+                        if (data.hasOwnProperty('report_customer_list') && Array.isArray(data.report_customer_list)) {
+                            $table.DataTable().clear().draw();
+                            $table.DataTable().rows.add(data.report_customer_list).draw();
+                            loadTotal();
+                        }
+                    }
+                }
+            )
+        });
+
+
+    });
+});

f82af302849fd77349be45dba7c6db475b765ffa
phuclh <phuclh@mtsolution.com.vn>
2023-11-23 12:05:06 +0700

    [feature/ms-274] chinh sua final_acceptance.js

--- apps/sales/acceptance/static/assets/sales/acceptance/js/final_acceptance.js
+++ apps/sales/acceptance/static/assets/sales/acceptance/js/final_acceptance.js
@@ -24,9 +24,9 @@ $(function () {
                                 if (row?.['sale_order_indicator']?.['indicator']?.['formula_data_show']) {
                                     formula = row?.['sale_order_indicator']?.['indicator']?.['formula_data_show'].replace(/"/g, "'");
                                 }
-                                return `<p class="table-row-indicator" data-id="${row?.['id']}" data-formula="${formula}">${row?.['sale_order_indicator']?.['indicator']?.['title'] ? row?.['sale_order_indicator']?.['indicator']?.['title'] : ''}</p>`;
+                                return `<p class="table-row-indicator" data-id="${row?.['id']}" data-code="${row?.['sale_order_indicator']?.['indicator']?.['code']}" data-is-delivery="${row?.['is_delivery']}" data-formula="${formula}">${row?.['sale_order_indicator']?.['indicator']?.['title'] ? row?.['sale_order_indicator']?.['indicator']?.['title'] : ''}</p>`;
                             } else {
-                                return `<p class="table-row-indicator" data-id="${row?.['id']}"></p>`;
+                                return `<p class="table-row-indicator" data-id="${row?.['id']}" data-code="${row?.['sale_order_indicator']?.['indicator']?.['code']}" data-is-delivery="${row?.['is_delivery']}"></p>`;
                             }
                         }
                     },
@@ -37,6 +37,8 @@ $(function () {
                                 return `<p>${row?.['sale_order']?.['code'] ? row?.['sale_order']?.['code'] : ''}</p>`;
                             } else if (row?.['is_payment'] === true) {
                                 return `<p>${row?.['payment']?.['code'] ? row?.['payment']?.['code'] : ''}</p>`;
+                            } else if (row?.['is_delivery'] === true) {
+                                return `<p>${row?.['delivery_sub']?.['code'] ? row?.['delivery_sub']?.['code'] : ''}</p>`;
                             }
                             else {
                                 return `<p></p>`;
@@ -50,6 +52,8 @@ $(function () {
                                 return `<p>${row?.['sale_order']?.['title'] ? row?.['sale_order']?.['title'] : ''}</p>`;
                             } else if (row?.['is_payment'] === true) {
                                 return `<p>${row?.['payment']?.['title'] ? row?.['payment']?.['title'] : ''}</p>`;
+                            } else if (row?.['is_delivery'] === true) {
+                                return `<p>${row?.['delivery_sub']?.['title'] ? row?.['delivery_sub']?.['title'] : ''}</p>`;
                             }
                             else {
                                 return `<p></p>`;
@@ -59,14 +63,22 @@ $(function () {
                     {
                         targets: 3,
                         render: (data, type, row) => {
-                            return `<span class="mask-money table-row-planed-value" data-init-money="${parseFloat(row?.['indicator_value'])}"></span>`;
+                            if (row?.['is_indicator'] === true) {
+                                return `<b><span class="mask-money table-row-planed-value" data-init-money="${parseFloat(row?.['indicator_value'])}"></span></b>`;
+                            } else {
+                                return `<span class="mask-money table-row-planed-value" data-init-money="${parseFloat(row?.['indicator_value'])}"></span>`;
+                            }
                         }
                     },
                     {
                         targets: 4,
                         render: (data, type, row) => {
-                            if (row?.['sale_order_indicator']?.['indicator']?.['title'] !== 'HR labor expense') {
-                                return `<span class="mask-money table-row-actual-value" data-init-money="${parseFloat(row?.['actual_value'])}"></span>`;
+                            if (row?.['sale_order_indicator']?.['indicator']?.['title'] !== 'HR labor expense' && row?.['is_delivery'] === false) {
+                                if (row?.['is_indicator'] === true) {
+                                    return `<b><span class="mask-money table-row-actual-value" data-init-money="${parseFloat(row?.['actual_value'])}"></span></b>`;
+                                } else {
+                                    return `<span class="mask-money table-row-actual-value" data-init-money="${parseFloat(row?.['actual_value'])}"></span>`;
+                                }
                             } else {
                                 return `<div class="row">
                                             <input 
@@ -74,6 +86,7 @@ $(function () {
                                                 class="form-control mask-money table-row-actual-value" 
                                                 value="${row?.['actual_value']}"
                                                 data-return-type="number"
+                                                data-is-delivery="${row?.['is_delivery']}"
                                             >
                                         </div>`;
                             }
@@ -88,7 +101,7 @@ $(function () {
                     {
                         targets: 6,
                         render: (data, type, row) => {
-                            return `<p>${row?.['rate_value']} %</p>`;
+                            return `<p class="table-row-rate-value" data-value="${row?.['rate_value']}">${row?.['rate_value']} %</p>`;
                         }
                     },
                     {
@@ -124,12 +137,17 @@ $(function () {
                                 $form[0].setAttribute('data-url', urlDetail);
                                 let so_row_data = {};
                                 let payment_row_data = {};
+                                let delivery_row_data = [];
                                 let revenueRow = null;
+                                let totalCostRow = null;
                                 for (let indicator of data.final_acceptance_list[0]?.['final_acceptance_indicator']) {
                                     if (indicator?.['is_indicator'] === true) {
                                         let newRow = $table.DataTable().row.add(indicator).draw().node();
                                         if (indicator?.['sale_order_indicator']?.['indicator']?.['code'] === 'IN0001') {
                                             revenueRow = newRow;
+                                        }
+                                        if (indicator?.['sale_order_indicator']?.['indicator']?.['code'] === 'IN0002') {
+                                            totalCostRow = newRow;
                                         }
                                     } else if (indicator?.['is_sale_order'] === true) {
                                         so_row_data = indicator;
@@ -139,12 +157,15 @@ $(function () {
                                         } else {
                                             payment_row_data[indicator['expense_item']['title']] = [indicator];
                                         }
-                                        // payment_row_data.push(indicator);
+                                    } else if (indicator?.['is_delivery'] === true) {
+                                        delivery_row_data.push(indicator);
                                     }
                                 }
                                 // Sale order row
                                 let newSORow = $table.DataTable().row.add(so_row_data).node();
-                                $(newSORow).detach().insertAfter(revenueRow);
+                                if (revenueRow) {
+                                    $(newSORow).detach().insertAfter(revenueRow);
+                                }
                                 // Payment rows
                                 for (let i = 0; i < $table[0].tBodies[0].rows.length; i++) {
                                     let row = $table[0].tBodies[0].rows[i];
@@ -159,12 +180,25 @@ $(function () {
                                                         $(newPaymentRow).detach().insertAfter(row);
                                                         newActualValue += payment_data?.['actual_value'];
                                                     }
-                                                    loadActualDifferentValue(row, newActualValue);
+                                                    loadActualDifferentRateValue(row, newActualValue);
                                                 }
                                             }
                                         }
                                     }
                                 }
+                                // Delivery rows
+                                let newActualValue = 0;
+                                let deliveryExistRow = null;
+                                for (let delivery_data of delivery_row_data) {
+                                    let newDeliRow = $table.DataTable().row.add(delivery_data).node();
+                                    if (!deliveryExistRow) {
+                                        deliveryExistRow = totalCostRow;
+                                    }
+                                    $(newDeliRow).detach().insertAfter(deliveryExistRow);
+                                    deliveryExistRow = newDeliRow;
+                                    newActualValue += delivery_data?.['actual_value'];
+                                }
+                                loadActualDifferentRateValue(totalCostRow, newActualValue);
                             $.fn.initMaskMoney2();
                             }
                             // $table.DataTable().rows.add(data.final_acceptance_list[0]?.['final_acceptance_indicator']).draw();
@@ -175,17 +209,43 @@ $(function () {
         }
         // loadFinalAcceptance();
 
-        function loadActualDifferentValue(row, newActualValue) {
+        function loadActualDifferentRateValue(row, newActualValue) {
             let elePlanedVal = row?.querySelector('.table-row-planed-value')?.getAttribute('data-init-money');
             let eleActual = row?.querySelector('.table-row-actual-value');
             let eleDifferent = row?.querySelector('.table-row-different-value');
-            if (elePlanedVal && eleActual && eleDifferent) {
+            let eleRate = row?.querySelector('.table-row-rate-value');
+            if (elePlanedVal && eleActual && eleDifferent && eleRate) {
+                // set actual value
                 eleActual.setAttribute('data-init-money', String(newActualValue));
+                // set different value
                 let differVal = parseFloat(elePlanedVal) - parseFloat(newActualValue);
+                eleDifferent.setAttribute('data-init-money', String(differVal));
+                // set rate value
+                let rateValue = parseFloat(eleRate.getAttribute('data-value'));
+                let revenueEle = $table[0].querySelector('.table-row-indicator[data-code="IN0001"]');
+                if (revenueEle) {
+                    let revenueRow = revenueEle.closest('tr');
+                    let revenuePlanedVal = revenueRow?.querySelector('.table-row-planed-value')?.getAttribute('data-init-money');
+                    eleRate.innerHTML = '';
+                    rateValue = ((parseFloat(newActualValue) / parseFloat(revenuePlanedVal)) * 100).toFixed(1);
+                    eleRate.innerHTML = String(rateValue) + ' %';
+                }
+                $.fn.initMaskMoney2();
+                // update updateIndicatorData Submit Data
+                let IDIndicator = row?.querySelector('.table-row-indicator')?.getAttribute('data-id');
+                updateIndicatorData[IDIndicator] = {'actual_value': newActualValue, 'different_value': differVal, 'rate_value': rateValue};
+            }
+        }
+
+        function changeActualValue(row) {
+            let elePlanedVal = row?.querySelector('.table-row-planed-value')?.getAttribute('data-init-money');
+            let eleDifferent = row?.querySelector('.table-row-different-value');
+            if (elePlanedVal && eleDifferent) {
+                let differVal = parseFloat(elePlanedVal) - parseFloat(this.value);
                 eleDifferent.setAttribute('data-init-money', String(differVal));
                 $.fn.initMaskMoney2();
                 let IDIndicator = row?.querySelector('.table-row-indicator')?.getAttribute('data-id');
-                updateIndicatorData[IDIndicator] = {'actual_value': newActualValue, 'different_value': differVal}
+                updateIndicatorData[IDIndicator] = {'actual_value': parseFloat(this.value), 'different_value': differVal}
             }
         }
 
@@ -211,20 +271,31 @@ $(function () {
         //     loadFinalAcceptance();
         // });
 
-        btnRefresh.on('click', function() {
-            loadFinalAcceptance();
+        btnRefresh.on('click', function () {
+            if (boxSO.val()) {
+                loadFinalAcceptance();
+            } else {
+                $.fn.notifyB({description: 'Please select sale order'}, 'failure');
+                return false
+            }
+            return true;
         });
 
         $table.on('change', '.table-row-actual-value', function () {
-            let row = this.closest('tr');
-            let elePlanedVal = row?.querySelector('.table-row-planed-value')?.getAttribute('data-init-money');
-            let eleDifferent = row?.querySelector('.table-row-different-value');
-            if (elePlanedVal && eleDifferent) {
-                let differVal = parseFloat(elePlanedVal) - parseFloat(this.value);
-                eleDifferent.setAttribute('data-init-money', String(differVal));
-                $.fn.initMaskMoney2();
-                let IDIndicator = row?.querySelector('.table-row-indicator')?.getAttribute('data-id');
-                updateIndicatorData[IDIndicator] = {'actual_value': parseFloat(this.value), 'different_value': differVal}
+            if (this.getAttribute('data-is-delivery') === 'true') {
+                let totalCostEle = $table[0].querySelector('.table-row-indicator[data-code="IN0002"]');
+                if (totalCostEle) {
+                    let totalCostRow = totalCostEle.closest('tr');
+                    let newActualValue = 0;
+                    for (let eleIndicator of $table[0].querySelectorAll('.table-row-indicator[data-is-delivery="true"]')) {
+                        let deliRow = eleIndicator.closest('tr');
+                        newActualValue += parseFloat(deliRow?.querySelector('.table-row-actual-value').value);
+                    }
+                    loadActualDifferentRateValue(totalCostRow, newActualValue);
+                }
+            } else {
+                let row = this.closest('tr');
+                changeActualValue(row);
             }
         });
 

ec6d5c0fcd4490a7a207798172135d104acd4374
trieunt <trieunt@mtsolution.com.vn>
2024-01-31 18:02:47 +0700

    [SIT] Class support print (need template before working)

--- misui/settings.py
+++ misui/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = \
         'apps.core.process',
         'apps.core.programme',
         'apps.core.comment',
+        'apps.core.extends',
     ] + [  # Another Application
         'apps.masterdata.saledata',
         'apps.masterdata.promotion',

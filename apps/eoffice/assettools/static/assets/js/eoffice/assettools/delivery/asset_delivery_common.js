5644c32245f59ebf9bfd2a3ea4c690d04cd938e1
quannm <quannm@mtsolution.com.vn>
2024-02-01 09:52:19 +0700

    [feature/update-content][workflow action] + update form create/update for request

--- apps/eoffice/assettools/static/assets/js/eoffice/assettools/delivery/asset_delivery_common.js
+++ apps/eoffice/assettools/static/assets/js/eoffice/assettools/delivery/asset_delivery_common.js
@@ -292,26 +292,7 @@ function submitHandleFunc() {
     }
 
     formData.attachments = $x.cls.file.get_val(formData.attachments, []),
-    $.fn.callAjax2({
-        'url': frm.dataUrl,
-        'method': frm.dataMethod,
-        'data': formData,
-    }).then(
-        (resp) => {
-            let data = $.fn.switcherResp(resp);
-            if (data && (data['status'] === 201 || data['status'] === 200)) {
-                $.fn.notifyB({description: $('#base-trans-factory').attr('data-success')}, 'success');
-                setTimeout(() => {
-                    window.location.replace($FormElm.attr('data-url-redirect'));
-                }, 1000);
-            }
-        }, (err) => {
-            setTimeout(() => {
-                WindowControl.hideLoading();
-            }, 1000)
-            $.fn.notifyB({description: err.data.errors}, 'failure');
-        }
-    )
+    WFRTControl.callWFSubmitForm(frm);
 }
 
 $(document).ready(function(){

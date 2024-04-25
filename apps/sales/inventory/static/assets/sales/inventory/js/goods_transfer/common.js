bb7239d24d77b3fb006cc6e1ce9ed5b43b7386ec
haind <haind@mtsolution.com.vn>
2024-04-25 18:30:57 +0700

    [432] update Goods Detail

--- apps/sales/inventory/static/assets/sales/inventory/js/goods_transfer/common.js
+++ apps/sales/inventory/static/assets/sales/inventory/js/goods_transfer/common.js
@@ -1,198 +1,175 @@
-let urlEle = $('#url-factory');
-let transEle = $('#trans-factory');
+const $tab_line_detail_datatable = $('#tab_line_detail_datatable')
+const $btn_add_row_line_detail = $('#btn-add-row-line-detail')
+const $trans_script = $('#trans-script')
+const $url_script = $('#url-script')
 
-class GoodsTransferLoadPage {
-    static loadAgency(ele, data) {
-        ele.initSelect2({
-            data: data,
-            dataParams: {
-                'account_types_mapped__account_type_order': 2
-            }
-        })
-    }
-
-    static loadWarehouse(ele, data, agency, wh_selected) {
-        ele.initSelect2({
-            data: data,
-            callbackDataResp(resp, keyResp) {
-                let list_result = []
-                resp.data[keyResp].map(function (item) {
-                    if (item.agency === agency && item.id !== wh_selected) {
-                        list_result.push(item)
+function loadDefaultTableLineDetail() {
+    if (!$.fn.DataTable.isDataTable('#tab_line_detail_datatable')) {
+        $tab_line_detail_datatable.DataTableDefault({
+            dom: '',
+            data: [],
+            columns: [
+                {
+                    data: '',
+                    className: 'wrap-text',
+                    render: (data, type, row) => {
+                        return ``;
                     }
-                })
-                return list_result
-            }
-        })
-    }
-
-    static loadProduct(ele, data, warehouse, list_selected) {
-        ele.initSelect2({
-            data: data,
-            'dataParams': {'warehouse_id': warehouse},
-            callbackDataResp(resp, keyResp) {
-                let list_result = [];
-                resp.data[keyResp].map(function (item) {
-                    if (!list_selected.includes(item.id)) {
-                        list_result.push(item)
+                },{
+                    data: '',
+                    className: 'wrap-text',
+                    render: (data, type, row) => {
+                        return ``;
                     }
-                })
-                return list_result
-            },
-            callbackTextDisplay: function (item) {
-                return item?.['product_data']?.['title'] || '';
-            },
-        })
+                },{
+                    data: '',
+                    className: 'wrap-text',
+                    render: (data, type, row) => {
+                        return ``;
+                    }
+                },{
+                    data: '',
+                    className: 'wrap-text',
+                    render: (data, type, row) => {
+                        return ``;
+                    }
+                },{
+                    data: '',
+                    className: 'wrap-text',
+                    render: (data, type, row) => {
+                        return ``;
+                    }
+                },{
+                    data: '',
+                    className: 'wrap-text',
+                    render: (data, type, row) => {
+                        return ``;
+                    }
+                },{
+                    data: '',
+                    className: 'wrap-text',
+                    render: (data, type, row) => {
+                        return ``;
+                    }
+                },{
+                    data: '',
+                    className: 'wrap-text',
+                    render: (data, type, row) => {
+                        return ``;
+                    }
+                },{
+                    data: '',
+                    className: 'wrap-text',
+                    render: (data, type, row) => {
+                        return ``;
+                    }
+                },
+            ],
+        });
     }
-
-    static loadDtbProduct(data) {
-        if (!$.fn.DataTable.isDataTable('#dtbProduct')) {
-            let dtb = $('#dtbProduct');
-            dtb.DataTableDefault({
-                data: data,
-                reloadCurrency: true,
-                columns: [
-                    {
-                        render: (data, type, row, meta) => {
-                            return `<select class="box-select-wh form-select" data-method="GET" data-url="${urlEle.data('url-warehouse')}" data-keyResp="warehouse_list"></select>`
-                        },
-                    }, {
-                        render: (data, type, row) => {
-                            return `<select class="box-select-product form-select" data-method="GET" data-url="${urlEle.data('url-warehouse-product')}" data-keyResp="warehouse_products_list"></select>`
-                        },
-                    }, {
-                        data: 'uom',
-                        className: 'wrap-text',
-                        render: (data, type, row) => {
-                            return `<span class="col-uom">${data.title}</span>`;
-                        },
-                    }, {
-                        data: 'quantity',
-                        className: 'wrap-text',
-                        render: (data, type, row) => {
-                            return `<input class="form-control col-quantity" value="${data}" />`;
-                        },
-                    }, {
-                        render: (data, type, row, meta) => {
-                            return `<select class="box-select-end-wh form-select" data-method="GET" data-url="${urlEle.data('url-warehouse')}" data-keyResp="warehouse_list"></select>`
-                        },
-                    }, {
-                        data: 'unit_cost',
-                        className: 'wrap-text',
-                        render: (data, type, row) => {
-                            return `<input class="form-control mask-money col-unit-cost" value="${data}" />`;
-                        },
-                    }, {
-                        data: 'subtotal',
-                        className: 'wrap-text',
-                        render: (data, type, row) => {
-                            return `<input class="form-control mask-money col-subtotal" readonly value="${data}" />`;
-                        },
-                    }, {
-                        className: 'wrap-text',
-                        render: (data, type, row) => {
-                            return `<a class="btn btn-icon btn-flush-dark btn-rounded flush-soft-hover btn-remove-row" ><span class="btn-icon-wrap"><span class="feather-icon"><i data-feather="trash-2"></i></span></span></a>`;
-
-                        },
-                    },
-                ],
-            });
-        }
-    }
-
-    static getListPrProductSelected(ele) {
-        let list_selected = []
-        $('#dtbProduct .box-select-product').not(ele).each(function () {
-            list_selected.push($(this).val());
-        })
-        return list_selected
-    }
-
-    static generateRowProduct(table) {
-        let data = {
-            'uom': {
-                'title': '',
-            },
-            'quantity': '',
-            'unit_cost': '',
-            'subtotal': '',
-        }
-        table.DataTable().row.add(data).draw().node();
-    }
-
-    static generateSubtotal(ele) {
-        let tr_current = ele.closest('tr');
-        let quantity = tr_current.find('.col-quantity').val();
-        let unit_cost = tr_current.find('.col-unit-cost').valCurrency();
-        tr_current.find('.col-subtotal').attr('value', quantity * unit_cost);
-        $.fn.initMaskMoney2();
-    }
-
-    static getDataForm(frmData) {
-        let rows = $('#dtbProduct tbody tr');
-        let list_product = [];
-        rows.each(function () {
-            let productEle = $(this).find('.box-select-product');
-            let product = SelectDDControl.get_data_from_idx(productEle, productEle.val());
-            let data = {
-                'warehouse_product': product.id,
-                'warehouse': $(this).find('.box-select-wh').val(),
-                'uom': product.uom,
-                'quantity': $(this).find('.col-quantity').val(),
-                'end_warehouse': $(this).find('.box-select-end-wh').val(),
-                'unit_cost': $(this).find('.col-unit-cost').valCurrency(),
-                'subtotal': $(this).find('.col-subtotal').valCurrency(),
-                'tax_data': product.tax_data,
-                'unit_price': product.unit_price,
-            }
-            list_product.push(data)
-        })
-        frmData['goods_transfer_datas'] = list_product;
-        frmData['system_status'] = 0;
-        if (frmData['warehouse_type'] === '0') {
-            delete frmData['agency']
-        }
-        return frmData
-    }
-
-    static loadGoodsTransferProductDatas(data){
-        let table = $('#dtbProduct');
-        data.map(function (item){
-            table.DataTable().row.add(item).draw().node();
-            let tr_current = table.find('tbody tr').last();
-            GoodsTransferLoadPage.loadWarehouse(tr_current.find('.box-select-wh'), item.warehouse, null, item.end_warehouse.id);
-            GoodsTransferLoadPage.loadWarehouse(tr_current.find('.box-select-end-wh'), item.end_warehouse, null, item.warehouse.id);
-            GoodsTransferLoadPage.loadProduct(tr_current.find('.box-select-product'), item.warehouse_product, item.warehouse.id, []);
-        })
-        table.find('select, input').prop('disabled', true)
-    }
-
-    static loadGoodsTransferDetail(frmDetail, pk) {
-        let url = frmDetail.data('url').format_url_with_uuid(pk);
-        let agencySelectEle = $('#box-select-agency');
-        $.fn.callAjax2({
-            'url': url,
-            'method': 'GET',
-        }).then((resp) => {
-            let data = $.fn.switcherResp(resp);
-            if (data) {
-                let detail = data?.['goods_transfer_detail'];
-                $x.fn.renderCodeBreadcrumb(detail);
-                $('[name="title"]').val(detail.title);
-                GoodsTransferLoadPage.loadAgency(agencySelectEle, detail.agency);
-                $('[name="date_transfer"]').val(detail?.['date_transfer']);
-                $('[name="system_status"]').val(detail?.['system_status']);
-                $('[name="note"]').val(detail?.['note']);
-                if (detail?.['goods_transfer_type'] === 0) {
-                    $('#inlineRadio1').prop('checked', true);
-                } else {
-                    $('#inlineRadio2').prop('checked', true);
-                    agencySelectEle.closest('.form-group').removeClass('hidden');
-                }
-                GoodsTransferLoadPage.loadGoodsTransferProductDatas(detail?.['goods_transfer_datas']);
-            }
-        })
-    }
-
-
 }
+
+loadDefaultTableLineDetail()
+
+function loadProduct(ele, data) {
+    ele.initSelect2({
+        placeholder: $trans_script.attr('data-trans-product-list'),
+        allowClear: true,
+        ajax: {
+            url: $url_script.attr('data-url-product-list') + `?warehouse_id=${ele.closest('tr').find('.from-wh').val()}`,
+            method: 'GET',
+        },
+        callbackDataResp: function (resp, keyResp) {
+            return resp.data[keyResp];
+        },
+        data: (data ? data : null),
+        keyResp: 'warehouse_products_list',
+        keyId: 'id',
+        keyText: 'product.title',
+    }).on('change', function () {
+        if ($(this).val()) {
+            let selected = SelectDDControl.get_data_from_idx(ele, ele.val())
+            ele.closest('tr').find('.uom').text(selected?.['uom']?.['title'])
+            ele.closest('tr').find('.quantity').attr('data-quantity-limit', selected?.['stock_amount'])
+        }
+        else {
+            ele.closest('tr').find('.uom').text('')
+        }
+    })
+}
+
+function loadFromWH(ele, data) {
+    ele.initSelect2({
+        placeholder: $trans_script.attr('data-trans-out-warehouse-list'),
+        allowClear: true,
+        ajax: {
+            url: $url_script.attr('data-url-warehouse-list') + '?is_dropship=False',
+            method: 'GET',
+        },
+        data: (data ? data : null),
+        keyResp: 'warehouse_list',
+        keyId: 'id',
+        keyText: 'title',
+    }).on('change', function () {
+        loadProduct(ele.closest('tr').find('.prd'))
+        loadTargetWH(ele.closest('tr').find('.to-wh'), null, $(this))
+        ele.closest('tr').find('.prd').empty()
+        ele.closest('tr').find('.to-wh').empty()
+    })
+}
+
+function loadTargetWH(ele, data, from_wh) {
+    ele.initSelect2({
+        placeholder: $trans_script.attr('data-trans-target-warehouse-list'),
+        allowClear: true,
+        ajax: {
+            url: $url_script.attr('data-url-warehouse-list') + '?is_dropship=False',
+            method: 'GET',
+        },
+        callbackDataResp: function (resp, keyResp) {
+            let result = [];
+            for (let i = 0; i < resp.data[keyResp].length; i++) {
+                if (resp.data[keyResp][i]?.['id'] !== from_wh.val()) {
+                    result.push(resp.data[keyResp][i])
+                }
+            }
+            return result;
+        },
+        data: (data ? data : null),
+        keyResp: 'warehouse_list',
+        keyId: 'id',
+        keyText: 'title',
+    }).on('change', function () {})
+}
+
+$btn_add_row_line_detail.on('click', function () {
+    $tab_line_detail_datatable.find('tbody .dataTables_empty').closest('tr').remove()
+    let index = $tab_line_detail_datatable.find('tbody tr').length + 1
+    let row_html = $(`<tr>
+        <td class="index">${index}</td>
+        <td><select class="from-wh form-select select2"></select></td>
+        <td><select class="prd form-select select2"></select></td>
+        <td><span class="uom"></span></td>
+        <td><input class="quantity form-control" type="number"></td>
+        <td><select class="to-wh form-select select2"></select></td>
+        <td><input class="unit-price form-control mask-money"></td>
+        <td><input class="subtotal-price form-control mask-money"></td>
+        <td class="text-center"><a href="#" class="btn-delete text-danger"><i class="bi bi-trash"></i></a></td>
+    </tr>`)
+    $tab_line_detail_datatable.find('tbody').append(row_html)
+    loadFromWH(row_html.find('.from-wh'))
+})
+
+$(document).on("click", '.btn-delete', function () {
+    $(this).closest('tr').remove();
+    $tab_line_detail_datatable.find('tbody tr').each(function (index) {
+        $(this).find('.index').text(index+1)
+    })
+});
+
+$(document).on("change", '.quantity', function () {
+    if (parseFloat($(this).val()) > parseFloat($(this).attr('data-quantity-limit'))) {
+        $(this).val(0)
+        $.fn.notifyB({description: `Input quantity > stock quantity (${$(this).attr('data-quantity-limit')})`}, 'warning')
+    }
+});
--- apps/sales/inventory/static/assets/sales/inventory/js/goods_transfer/detail.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    let pk = $.fn.getPkDetail();
-    let frmDetail = $('#frmDetail');
-    GoodsTransferLoadPage.loadDtbProduct([]);
-    GoodsTransferLoadPage.loadGoodsTransferDetail(frmDetail, pk);
-})

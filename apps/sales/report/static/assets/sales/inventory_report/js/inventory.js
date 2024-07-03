f6e977d3528f410aea85a6163a40e427de53a3f5
haind <haind@mtsolution.com.vn>
2024-07-04 01:25:11 +0700

    [433]: Project

--- apps/sales/report/static/assets/sales/inventory_report/js/inventory.js
+++ apps/sales/report/static/assets/sales/inventory_report/js/inventory.js
@@ -444,7 +444,9 @@ $(document).ready(function () {
                 // group project
                 if (sale_order_code_list.length === 0) {
                     table.find("tr th:eq(0)").prop('hidden', true)
-                    table.find("tr td:eq(0)").prop('hidden', true)
+                    table.find("tr").each(function () {
+                        $(this).find('td:eq(0)').prop('hidden', true)
+                    })
                 }
                 for (const so_code_class of sale_order_code_list) {
                     let number_row = table.find(`.${so_code_class}`).length + 1

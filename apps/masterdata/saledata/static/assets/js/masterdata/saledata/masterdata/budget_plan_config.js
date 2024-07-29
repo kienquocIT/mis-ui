ce95fadd2566fdba567c4c58a1580db321b36b9e
haind <haind@mtsolution.com.vn>
2024-07-29 09:59:32 +0700

    [435]: Budget Plan Config

--- apps/masterdata/saledata/static/assets/js/masterdata/saledata/masterdata/budget_plan_config.js
+++ apps/masterdata/saledata/static/assets/js/masterdata/saledata/masterdata/budget_plan_config.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
                     dataSrc: function (resp) {
                         let data = $.fn.switcherResp(resp);
                         if (data) {
+                            console.log(resp.data['budget_plan_config'])
                             return resp.data['budget_plan_config'] ? resp.data['budget_plan_config'] : [];
                         }
                         return [];
@@ -35,13 +36,61 @@ $(document).ready(function () {
                     {
                         render: (data, type, row) => {
                             let html = ''
+                            if (row?.['can_view_company'] || row?.['can_lock_plan']) {
+                                html += `
+                                    <span class="w-10 mb-2 badge-sm badge badge-pill badge-primary">${row?.['company']?.['code']}</span>
+                                    <label class="w-40 ml-2 text-primary fw-bold">${trans_script.attr('data-trans-company')}</label>
+                                    <br>
+                                `
+                            }
                             for (const gr of row?.['group_allowed']) {
-                                html += `<span class="w-10 mb-2 badge-sm badge badge-pill badge-soft-secondary">${gr?.['group']?.['code']}</span>
-                                    <label class="w-40 ml-2 text-mute">${gr?.['group']?.['title']}</label>
+                                html += `
+                                    <span class="w-10 mb-2 badge-sm badge badge-pill badge-soft-primary">${gr?.['group']?.['code']}</span>
+                                    <label class="w-40 ml-2 text-primary">${gr?.['group']?.['title']}</label>
+                                    <br>
+                                `
+                            }
+                            return html;
+                        }
+                    },
+                    {
+                        className: '',
+                        render: (data, type, row) => {
+                            let html = ''
+                            if (row?.['can_view_company']) {
+                                html += `<div class="form-check form-switch form-check-inline ml-3 mr-3">
+                                        <input type="checkbox" class="form-check-input" disabled checked>
+                                        <label class="form-check-label">${trans_script.attr('data-trans-can-view-company')}</label>
+                                    </div>
+                                    <br>
+                                `;
+                            }
+                            for (const gr of row?.['group_allowed']) {
+                                html += `
                                     <div class="form-check form-switch form-check-inline ml-3 mr-3">
                                         <input type="checkbox" class="form-check-input" disabled ${gr?.['can_view'] ? 'checked' : ''}>
                                         <label class="form-check-label">${trans_script.attr('data-trans-view')}</label>
                                     </div>
+                                    <br>
+                                `
+                            }
+                            return html;
+                        }
+                    },
+                    {
+                        className: '',
+                        render: (data, type, row) => {
+                            let html = ''
+                            if (row?.['can_lock_plan']) {
+                                html += `<div class="form-check form-switch form-check-inline ml-3">
+                                        <input type="checkbox" class="form-check-input" disabled checked>
+                                        <label class="form-check-label">${trans_script.attr('data-trans-can-lock-plan')}</label>
+                                    </div>
+                                    <br>
+                                `;
+                            }
+                            for (const gr of row?.['group_allowed']) {
+                                html += `
                                     <div class="form-check form-switch form-check-inline ml-3">
                                         <input type="checkbox" class="form-check-input" disabled ${gr?.['can_edit'] ? 'checked' : ''}>
                                         <label class="form-check-label">${trans_script.attr('data-trans-edit')}</label>
@@ -127,258 +176,6 @@ $(document).ready(function () {
     }
     loadModalTable();
 
-    // Can view company budget plan
-
-    function loadModalTableCanViewCompanyBudgetPlan(data_selected=[]) {
-        if (!$.fn.DataTable.isDataTable('#can-view-company-table')) {
-            $('#can-view-company-table').DataTableDefault({
-                useDataServer: true,
-                rowIdx: true,
-                paging: false,
-                ajax: {
-                    url: url_script.attr('data-url-emp-list'),
-                    type: 'GET',
-                    dataSrc: function (resp) {
-                        let data = $.fn.switcherResp(resp);
-                        if (data) {
-                            return resp.data['employee_list'] ? resp.data['employee_list'] : [];
-                        }
-                        return [];
-                    },
-                },
-                columns: [
-                    {
-                        className: 'w-5',
-                        render: () => {
-                            return ``;
-                        }
-                    },
-                    {
-                        className: 'w-20 text-center',
-                        render: (data, type, row) => {
-                            return `<span class="badge badge-pill badge-soft-primary w-80">${row?.['code']}</span>`;
-                        }
-                    },
-                    {
-                        className: 'w-45',
-                        render: (data, type, row) => {
-                            return `<span class="text-primary">${row?.['full_name']}</span>`;
-                        }
-                    },
-                    {
-                        className: 'text-center w-30',
-                        render: (data, type, row) => {
-                            let checked = data_selected.includes(row?.['id']) ? 'checked' : ''
-                            return `
-                                <div class="form-check form-switch mb-1">
-                                    <input data-id="${row?.['id']}" ${checked} type="checkbox" class="form-check-input can-view-company">
-                                    <label class="form-check-label"></label>
-                                </div>
-                            `;
-                        }
-                    },
-                ],
-            });
-        }
-    }
-
-    function loadEmpCanViewCompanyBP() {
-        let dataParam = {}
-        let url_loaded = $.fn.callAjax2({
-            url: url_script.attr('data-url-emp-list-can-view-company-bp'),
-            data: dataParam,
-            method: 'GET'
-        }).then(
-            (resp) => {
-                let data = $.fn.switcherResp(resp);
-                if (data && typeof data === 'object' && data.hasOwnProperty('list_can_view_company_budget_plan')) {
-                    return data?.['list_can_view_company_budget_plan'];
-                }
-                return {};
-            },
-            (errs) => {
-                console.log(errs);
-            }
-        )
-
-        Promise.all([url_loaded]).then(
-            (resp) => {
-                let emp_id_list_can_view = []
-                for (const emp of resp[0]) {
-                    emp_id_list_can_view.push(emp?.['employee_allowed']?.['id'])
-                }
-                loadModalTableCanViewCompanyBudgetPlan(emp_id_list_can_view);
-            }
-        )
-    }
-
-    $('#can-view-company-btn').on('click', function () {
-        loadEmpCanViewCompanyBP()
-    })
-
-    $('#save-can-view-company').on('click', function () {
-        let emp_id_can_view_company_bp = []
-        $('#can-view-company-table tbody tr').each(function () {
-            if ($(this).find('.can-view-company').prop('checked')) {
-                emp_id_can_view_company_bp.push($(this).find('.can-view-company').attr('data-id'))
-            }
-        })
-
-        WindowControl.showLoading();
-        let ajaxSetup = {
-            url: url_script.attr('data-url-config-list'),
-            method: 'GET',
-            data: {
-                'emp_id_can_view_company_bp': JSON.stringify(emp_id_can_view_company_bp)
-            },
-        }
-        $.fn.callAjax2(ajaxSetup).then(
-            (resp) => {
-                let data = $.fn.switcherResp(resp);
-                if (data) {
-                    $.fn.notifyB({description: 'Update successfully!'}, 'success');
-                    setTimeout(() => {
-                        location.reload();
-                    }, 1000)
-                }
-            },
-            (errs) => {
-                setTimeout(
-                    () => {
-                        WindowControl.hideLoading();
-                    },
-                    1000
-                )
-                $.fn.notifyB({description: errs.data.errors}, 'failure');
-            })
-    })
-
-    // Can lock budget plan
-
-    function loadModalTableCanLockBudgetPlan(data_selected=[]) {
-        if (!$.fn.DataTable.isDataTable('#can-lock-bp-table')) {
-            $('#can-lock-bp-table').DataTableDefault({
-                useDataServer: true,
-                rowIdx: true,
-                paging: false,
-                ajax: {
-                    url: url_script.attr('data-url-emp-list'),
-                    type: 'GET',
-                    dataSrc: function (resp) {
-                        let data = $.fn.switcherResp(resp);
-                        if (data) {
-                            return resp.data['employee_list'] ? resp.data['employee_list'] : [];
-                        }
-                        return [];
-                    },
-                },
-                columns: [
-                    {
-                        className: 'w-5',
-                        render: () => {
-                            return ``;
-                        }
-                    },
-                    {
-                        className: 'w-20 text-center',
-                        render: (data, type, row) => {
-                            return `<span class="badge badge-pill badge-soft-primary w-80">${row?.['code']}</span>`;
-                        }
-                    },
-                    {
-                        className: 'w-45',
-                        render: (data, type, row) => {
-                            return `<span class="text-primary">${row?.['full_name']}</span>`;
-                        }
-                    },
-                    {
-                        className: 'text-center w-30',
-                        render: (data, type, row) => {
-                            let checked = data_selected.includes(row?.['id']) ? 'checked' : ''
-                            return `
-                                <div class="form-check form-switch mb-1">
-                                    <input data-id="${row?.['id']}" ${checked} type="checkbox" class="form-check-input can-lock-bp">
-                                    <label class="form-check-label"></label>
-                                </div>
-                            `;
-                        }
-                    },
-                ],
-            });
-        }
-    }
-
-    function loadEmpCanLockBP() {
-        let dataParam = {}
-        let url_loaded = $.fn.callAjax2({
-            url: url_script.attr('data-url-emp-list-can-lock-bp'),
-            data: dataParam,
-            method: 'GET'
-        }).then(
-            (resp) => {
-                let data = $.fn.switcherResp(resp);
-                if (data && typeof data === 'object' && data.hasOwnProperty('list_can_lock_budget_plan')) {
-                    return data?.['list_can_lock_budget_plan'];
-                }
-                return {};
-            },
-            (errs) => {
-                console.log(errs);
-            }
-        )
-
-        Promise.all([url_loaded]).then(
-            (resp) => {
-                let emp_id_list_can_lock = []
-                for (const emp of resp[0]) {
-                    emp_id_list_can_lock.push(emp?.['employee_allowed']?.['id'])
-                }
-                loadModalTableCanLockBudgetPlan(emp_id_list_can_lock);
-            }
-        )
-    }
-
-    $('#can-lock-bp-btn').on('click', function () {
-        loadEmpCanLockBP()
-    })
-
-    $('#save-can-lock-bp').on('click', function () {
-        let emp_id_can_lock_bp = []
-        $('#can-lock-bp-table tbody tr').each(function () {
-            if ($(this).find('.can-lock-bp').prop('checked')) {
-                emp_id_can_lock_bp.push($(this).find('.can-lock-bp').attr('data-id'))
-            }
-        })
-
-        WindowControl.showLoading();
-        let ajaxSetup = {
-            url: url_script.attr('data-url-config-list'),
-            method: 'GET',
-            data: {
-                'emp_id_can_lock_bp': JSON.stringify(emp_id_can_lock_bp)
-            },
-        }
-        $.fn.callAjax2(ajaxSetup).then(
-            (resp) => {
-                let data = $.fn.switcherResp(resp);
-                if (data) {
-                    $.fn.notifyB({description: 'Update successfully!'}, 'success');
-                    setTimeout(() => {
-                        location.reload();
-                    }, 1000)
-                }
-            },
-            (errs) => {
-                setTimeout(
-                    () => {
-                        WindowControl.hideLoading();
-                    },
-                    1000
-                )
-                $.fn.notifyB({description: errs.data.errors}, 'failure');
-            })
-    })
-
     function LoadEmp(ele, data) {
         ele.initSelect2({
             allowClear: true,
@@ -428,6 +225,8 @@ $(document).ready(function () {
             })
         })
         frm.dataForm['employee_allowed'] = employee_allowed_id
+        frm.dataForm['can_view_company'] = $('#can-view-company').prop('checked')
+        frm.dataForm['can_lock_plan'] = $('#can-lock-plan').prop('checked')
         frm.dataForm['group_allowed_list'] = group_allowed_list
 
         console.log(frm)

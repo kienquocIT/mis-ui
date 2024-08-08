10fc13e0ca98dc5c9d8a0b99a271827f3bc77c44
haind <haind@mtsolution.com.vn>
2024-08-08 15:27:56 +0700

    [436]: Project + update UI

--- apps/shared/templates_map_workflow/cashoutflow_advance.py
+++ apps/shared/templates_map_workflow/cashoutflow_advance.py
@@ -16,9 +16,6 @@ ADVANCE_DATA_MAP = {
     'return_date': FieldMapCommon(
         name_mapping=['return_date'],
     ).data,
-    'money_gave': FieldMapCommon(
-        name_mapping=['money_gave'],
-    ).data,
     'expense_valid_list': FieldMapCommon(
         name_mapping=['expense_valid_list'],
     ).data,

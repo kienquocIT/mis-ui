2259b5caadca483f259fb3ce10f5725bedc2ab50
trieunt <trieunt@mtsolution.com.vn>
2024-08-14 10:36:01 +0700

    [SIT] Add captcha in Forgot Password

--- runserver_plus.sh
+++ runserver_plus.sh
@@ -2,4 +2,4 @@
 
 # https://werkzeug.palletsprojects.com/en/3.0.x/serving/
 
-python manage.py runserver_plus --cert-file ./statics/ssl-fake/cert.pem --key-file ./statics/ssl-fake/key.pem 0.0.0.0:80
+python manage.py runserver_plus --cert-file ./statics/ssl-fake/cert.pem --key-file ./statics/ssl-fake/key.pem 0.0.0.0:443

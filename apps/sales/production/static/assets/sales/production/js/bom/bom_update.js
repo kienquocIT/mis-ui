4f57c8af1934b4d2850c338cb605a87102cbfcbf
haind <haind@mtsolution.com.vn>
2024-08-26 09:43:08 +0700

    [436]: update BOM

--- apps/sales/production/static/assets/sales/production/js/bom/bom_update.js
+++ apps/sales/production/static/assets/sales/production/js/bom/bom_update.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
     new BOMHandle().load();
 
     LoadDetailBOM('update');
+
+    $('#form-detail-bom').submit(function (event) {
+        event.preventDefault();
+        let form = new BOMHandle().combinesDataForProductionBOM($(this));
+        if (form) {
+            WFRTControl.callWFSubmitForm(form);
+        }
+    })
 })

bc062a589d255aa5a40900ec9616ea47f412384e
haind <haind@mtsolution.com.vn>
2024-10-06 22:52:27 +0700

    [feature/ms-436]: Add Difference Column for Production GIS

--- apps/masterdata/saledata/views/balance_init.py
+++ apps/masterdata/saledata/views/balance_init.py
@@ -4,7 +4,7 @@ from django.views import View
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-from apps.shared import mask_view, ApiURL, ServerAPI, MDConfigMsg, PermCheck
+from apps.shared import mask_view, ApiURL, ServerAPI, MDConfigMsg, PermCheck, SaleMsg
 
 
 class BalanceInitList(View):
@@ -37,3 +37,39 @@ class BalanceInitListAPI(APIView):
         params = request.query_params.dict()
         resp = ServerAPI(user=request.user, url=ApiURL.BALANCE_INIT_LIST).get(params)
         return resp.auto_return(key_success='balance_init_list')
+
+    @mask_view(
+        auth_require=True,
+        is_api=True,
+    )
+    def post(self, request, *arg, **kwargs):
+        resp = ServerAPI(user=request.user, url=ApiURL.BALANCE_INIT_LIST).post(request.data)
+        if resp.state:
+            resp.result['message'] = SaleMsg.BALANCE_INIT_CREATE
+            return resp.result, status.HTTP_201_CREATED
+        return resp.auto_return()
+
+
+class BalanceInitListAPIImportDB(APIView):
+    permission_classes = [IsAuthenticated] # noqa
+
+    @mask_view(
+        auth_require=True,
+        is_api=True,
+    )
+    def post(self, request, *arg, **kwargs):
+        resp = ServerAPI(user=request.user, url=ApiURL.BALANCE_INIT_LIST_IMPORT_DB).post(request.data)
+        if resp.state:
+            resp.result['message'] = SaleMsg.BALANCE_INIT_CREATE
+            return resp.result, status.HTTP_201_CREATED
+        return resp.auto_return()
+
+
+class ImportBalanceInitDBAPIViews(APIView):
+    @mask_view(
+        auth_require=True,
+        is_api=True,
+    )
+    def put(self, request, pk, *args, **kwargs):
+        resp = ServerAPI(user=request.user, url=ApiURL.PERIODS_CONFIG_DETAIL.fill_key(pk=pk)).put(request.data)
+        return resp.auto_return(key_success='periods_detail')
--- statics/assets/js/import-datatable.js
+++ statics/assets/js/import-datatable.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     const preview_table = $('#preview-table');
+    let THIS_ROW = null
+    const trans_db_script = $('#import-db-trans-script')
 
     $('#input-excel').on('change', function () {
         const input = event.target;
@@ -18,8 +20,11 @@ $(document).ready(function () {
         reader.readAsArrayBuffer(input.files[0]);
     });
 
+    $('.btn-import-datatable-from-excel').on('click', function () {
+        THIS_ROW = $(this).closest('.row')
+    })
+
     function displayExcelData(data) {
-        console.log(data)
         if (data.length > 1) {
             let header = data[0]
             let from_index = $('#from-index').val() ? parseInt($('#from-index').val()) : null
@@ -33,14 +38,14 @@ $(document).ready(function () {
                 for (let i = 0; i < header.length; i++) {
                     ths += `<th>${header[i]}</th>`
                 }
-                preview_table.find('thead').append(`<tr>${ths}</tr>`)
+                preview_table.find('thead').append(`<tr>${ths}<th class="w-10 text-center">${trans_db_script.attr('data-trans-status')}</th></tr>`)
 
                 for (let i = from_index; i <= to_index; i++) {
                     let tds = ``
                     for (let j = 0; j < data[i].length; j++) {
                         tds += `<td>${data[i][j]}</td>`
                     }
-                    preview_table.find('tbody').append(`<tr>${tds}</tr>`)
+                    preview_table.find('tbody').append(`<tr>${tds}<td class="w-10 text-center"><i hidden class="status-ok text-success fw-bold bi bi-check2"></i><i hidden class="status-error text-danger fw-bold bi bi-x-lg"></i></td></tr>`)
                 }
             }
         }
@@ -48,4 +53,63 @@ $(document).ready(function () {
             $.fn.notifyB({description: "File is empty!"}, 'warning')
         }
     }
+
+    function combinesDataImportDB(frmEle, data) {
+        let frm = new SetupFormSubmit($(frmEle));
+
+        return {
+            url: frm.dataUrl,
+            method: frm.dataMethod,
+            data: data,
+            urlRedirect: frm.dataUrlRedirect,
+        };
+    }
+
+    $('#import-db-form').submit(function (event) {
+        event.preventDefault();
+
+        let no_new_row = preview_table.find('tbody tr').length
+        let count = 0
+        preview_table.find('tbody tr').each(function() {
+            let data = combinesDataImportDB(
+                $('#import-db-form'),
+                {
+                    'balance_data': {
+                        'product_code': $(this).find('td:eq(0)').text(),
+                        'warehouse_code': $(this).find('td:eq(2)').text(),
+                        'quantity': $(this).find('td:eq(3)').text(),
+                        'value': $(this).find('td:eq(4)').text(),
+                        'data_sn': JSON.parse('[]'),
+                        'data_lot': JSON.parse('[]'),
+                    }
+                }
+            );
+            // console.log(data)
+            if (data) {
+                $.fn.callAjax2(data)
+                    .then(
+                        (resp) => {
+                            let data = $.fn.switcherResp(resp);
+                            if (data) {
+                                $.fn.notifyB({description: "Successfully"}, 'success')
+                                count += 1
+                                if (count === no_new_row) {
+                                    $(this).find('.status-ok').prop('hidden', false)
+                                    $(this).find('.status-error').prop('hidden', true)
+                                }
+                                else {
+                                    $(this).find('.status-ok').prop('hidden', false)
+                                    $(this).find('.status-error').prop('hidden', true)
+                                }
+                            }
+                        },
+                        (errs) => {
+                            $(this).find('.status-error').prop('hidden', false)
+                            $(this).find('.status-ok').prop('hidden', true)
+                            $.fn.notifyB({description: `Can not import this row.`}, 'failure');
+                        }
+                    )
+            }
+        })
+    })
 })

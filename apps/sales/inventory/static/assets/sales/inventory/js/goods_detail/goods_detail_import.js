db38677956fce789378a9e396634c78a3bfd3d4d
namviet <namvietnguyenn@gmail.com>
2024-10-08 16:35:20 +0700

    [feature/ms-001] add goods_detail_import

---
+++ apps/sales/inventory/static/assets/sales/inventory/js/goods_detail/goods_detail_import.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    let number_free_row = 0
+
+    $('#btn-import-datatable-from-excel').on('click', function () {
+        number_free_row = 0
+        $('#table-serial tbody tr').each(function () {
+            if (!$(this).find('.vendor_serial_number').val()) {
+                number_free_row += 1
+            }
+        })
+        $('#to-index').val(parseInt($('#from-index').val()) + number_free_row - 1).prop('disabled', true).prop('readonly', true)
+    })
+
+    $('#from-index').on('change', function () {
+        if ($(this).val()) {
+            $('#to-index').val(parseInt($('#from-index').val()) + number_free_row - 1).prop('disabled', true).prop('readonly', true)
+        }
+    })
+})

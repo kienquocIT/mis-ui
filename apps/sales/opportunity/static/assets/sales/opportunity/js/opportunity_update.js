36bf5baa81c55444fac47653851c663f861dd651
quannm <quannm@mtsolution.com.vn>
2024-10-17 17:58:11 +0700

    [feature/ms-470][project] + fix bug create task on opportunity redirect + business trip check date valid + fix bug home page + fix bug show chart project per year

--- apps/sales/opportunity/static/assets/sales/opportunity/js/opportunity_update.js
+++ apps/sales/opportunity/static/assets/sales/opportunity/js/opportunity_update.js
@@ -1436,7 +1436,14 @@ $(document).ready(function () {
                 }
 
                 // for task
-                Task_in_opps.init(opportunity_detail_data, OpportunityActivity.loadDblActivityLogs)
+                const $form = $('#formOpportunityTask')
+                Task_in_opps.init(opportunity_detail_data)
+                SetupFormSubmit.validate($form, {
+                    errorClass: 'is-invalid cl-red',
+                    submitHandler: function (){
+                        TaskSubmitFuncOpps($form, OpportunityActivity.loadDblActivityLogs)
+                    }
+                })
 
                 // event create related features
                 $('#dropdown-menu-relate-app #create-advance-payment-shortcut').on('click', function () {

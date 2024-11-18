829b042dcac4ff85556cf3d8792a1f6a7724419b
haind <haind@mtsolution.com.vn>
2024-11-18 15:14:20 +0700

    [feature/ms-437]: update Inventory Features + UI Contact

--- apps/sales/cashoutflow/static/assets/js/payment/payment_create.js
+++ apps/sales/cashoutflow/static/assets/js/payment/payment_create.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
     new $x.cls.file($('#attachment')).init({'name': 'attachment'});
 
-    PaymentHandle.LoadPage()
+    const urlParams = new URLSearchParams(window.location.search)
+    let opportunity_json= urlParams.get('opp_mapped')
+    let opportunity = opportunity_json ? JSON.parse(decodeURIComponent(opportunity_json)) : null
+    let quotation_json= urlParams.get('quotation_object')
+    let quotation = quotation_json ? JSON.parse(decodeURIComponent(quotation_json)) : null
+    let sale_order_json= urlParams.get('sale_order_object')
+    let sale_order = sale_order_json ? JSON.parse(decodeURIComponent(sale_order_json)) : null
+    opportunity['quotation'] = quotation
+    opportunity['sale_order'] = sale_order
+
+    PaymentHandle.LoadPage(opportunity)
     WFRTControl.setWFInitialData('payment', 'POST')
 
     let form_validator = $('#form-create-payment').validate({

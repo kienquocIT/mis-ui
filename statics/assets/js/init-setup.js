d0a41c9806339401ca09cbc15fd76f2ae34a4791
quannm <quannm@mtsolution.com.vn>
2024-11-26 17:57:13 +0700

    [feature/ms-475][HRM] + commit HRM employee info

--- statics/assets/js/init-setup.js
+++ statics/assets/js/init-setup.js
@@ -7087,6 +7087,7 @@ class FileControl {
                                     ${opts.required ? "required" : ""}
                                 />
                             `);
+                            groupEle.addClass('dm-uploader-initializer');
 
                             // init table file cloud
                             clsThis.ui_on_show_file_cloud();

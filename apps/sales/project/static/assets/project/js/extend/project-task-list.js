d6518ca1edb50551ef9133f21da5ec57369f7722
quannm <quannm@mtsolution.com.vn>
2024-12-12 15:57:11 +0700

    [hotfix/ms-470][project] + add filter task__task_status_id, member_id in project-task-list.js

--- apps/sales/project/static/assets/project/js/extend/project-task-list.js
+++ apps/sales/project/static/assets/project/js/extend/project-task-list.js
@@ -88,8 +88,8 @@ class handle_tasks_cls {
                         if (txtSearch.length > 0)
                             params['search'] = txtSearch
                         params['project_id'] = $('#filter_project_id').val() || ''
-                        params['task_status'] = $('#filter_task_status').val() || ''
-                        params['employee_inherit'] = $('#filter_employee_id').val() || ''
+                        params['task__task_status_id'] = $('#filter_task_status').val() || ''
+                        params['member_id'] = $('#filter_employee_id').val() || ''
                         params['is_ajax'] = true;
                         return params
                     },

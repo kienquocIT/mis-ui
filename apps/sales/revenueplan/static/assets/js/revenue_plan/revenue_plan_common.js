d91c2783c74ea3b4d48e7c436ffc2ce495234b33
haind <haind@mtsolution.com.vn>
2025-01-07 02:45:40 +0700

    [ft/442] update Account Page

--- apps/sales/revenueplan/static/assets/js/revenue_plan/revenue_plan_common.js
+++ apps/sales/revenueplan/static/assets/js/revenue_plan/revenue_plan_common.js
@@ -360,7 +360,6 @@ $(document).on("change", '.month-target', function () {
         sum_year_target_company += parseFloat(sum_year)
     })
 
-
     for (let i = 0; i < 12; i++) {
         revenuePlanTable.find(`thead .sum-company-m${i+1}`).attr('value', sum_month_target_company[i])
     }
@@ -592,8 +591,8 @@ function UpdateTablePlan(group_employee_list, group_selected) {
             revenuePlanTable.find('.ytarget-td').each(function () {
                 if ($(this).html() === '') {
                     $(this).append(`
-                        <input class="mb-1 mask-money form-control text-primary yeartargetvalue" value="0">
-                        <input class="mask-money form-control text-secondary yeartargetvalue-profit" value="0">
+                        <input readonly disabled class="mb-1 mask-money form-control text-primary yeartargetvalue" value="0">
+                        <input readonly disabled class="mask-money form-control text-secondary yeartargetvalue-profit" value="0">
                     `)
                 }
             })

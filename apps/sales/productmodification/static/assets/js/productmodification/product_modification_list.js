4cec1fe3886f16d814e62196fbf77de62e6c5f2f
haind <haind@mtsolution.com.vn>
2025-06-15 23:26:52 +0700

    [ft/440]: PM

--- apps/sales/productmodification/static/assets/js/productmodification/product_modification_list.js
+++ apps/sales/productmodification/static/assets/js/productmodification/product_modification_list.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    function loadARInvoiceList() {
+    function loadPMList() {
         if (!$.fn.DataTable.isDataTable('#datatable_product_modification_list')) {
             let dtb = $('#datatable_product_modification_list');
             let frm = new SetupFormSubmit(dtb);
@@ -82,31 +82,5 @@ $(document).ready(function () {
         }
     }
 
-    loadARInvoiceList();
-
-    $('#reload-invoice-status-btn').on('click', function () {
-        WindowControl.showLoading();
-        let url_loaded = $('#datatable_ar_invoice_list').attr('data-url') + `?update_status=true`
-        $.fn.callAjax(url_loaded, 'GET').then(
-            (resp) => {
-                let data = $.fn.switcherResp(resp);
-                if (data) {
-                    WindowControl.hideLoading();
-                    $.fn.notifyB({description: "Update successfully"}, 'success')
-                    setTimeout(() => {
-                        window.location.replace($('#datatable_ar_invoice_list').attr('data-url-redirect'));
-                        location.reload.bind(location);
-                    }, 1000);
-                }
-            },
-            (errs) => {
-                setTimeout(
-                    () => {
-                        WindowControl.hideLoading();
-                    },
-                    1000
-                )
-                $.fn.notifyB({description: errs.data.errors}, 'failure');
-            })
-    })
+    loadPMList();
 })

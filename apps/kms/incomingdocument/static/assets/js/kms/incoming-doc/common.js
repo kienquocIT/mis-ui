ce11906dc55d96690796abf21487651bab2a8dc4
Windows-key <giacanemail@gmail.com>
2025-06-17 09:12:59 +0700

    [feature/ms-242]: update UI structure for Incoming Document

---
+++ apps/kms/incomingdocument/static/assets/js/kms/incoming-doc/common.js
@@ -0,0 +1,81 @@
+// Declare elements in Page
+class IncomingDocElements {
+    constructor() {
+        this.$titleEle = $('#txt_title')
+        this.$descriptionEle = $('#textarea_remark')
+        this.$senderEle = $('#sender')
+        this.$docTypeEle = $('#select-box-doc_type')
+        this.$contentGroupEle = $('#select-box-content_group')
+        this.$effectiveDateEle = $('#kms_effective_date')
+        this.$folderEle = $('#kms_folder')
+    }
+}
+const pageElements = new IncomingDocElements();
+
+// Load data
+class IncomingDocLoadDataHandle {
+    static initPage() {
+        // init selection
+        FormElementControl.loadInitS2(pageElements.$docTypeEle);
+        FormElementControl.loadInitS2(pageElements.$contentGroupEle);
+        FormElementControl.loadInitS2(pageElements.$folderEle);
+
+        // init date
+        $('.date-picker').each(function () {
+            DateTimeControl.initDatePicker(this);
+        });
+    }
+    static combineData(formEle) {
+        let frm = new SetupFormSubmit($(formEle));
+        frm.dataForm['title'] = pageElements.$titleEle.val()
+        frm.dataForm['description'] = pageElements.$descriptionEle.val() || null
+        frm.dataForm['sender'] = pageElements.$senderEle.val()
+        frm.dataForm['effective_date'] = moment(pageElements.$effectiveDateEle.val(), 'DD/MM/YYYY').format('YYYY-MM-DD')
+        return frm
+    }
+}
+
+// DataTable
+class IncomingDocDtbHandle {
+
+}
+
+// Load Editor
+class loadEditor{
+    init(){
+        const $textArea = $('#textarea_remark');
+        $textArea.tinymce({
+            height: 500,
+            menubar: false,
+            plugins: ['columns', 'print', 'preview', 'paste', 'importcss', 'searchreplace', 'autolink', 'autosave',
+                'save', 'directionality', 'code', 'visualblocks', 'visualchars', 'fullscreen', 'image', 'link', 'media',
+                'codesample', 'table', 'charmap', 'hr', 'pagebreak', 'nonbreaking', 'anchor', 'toc',
+                'insertdatetime', 'advlist', 'lists', 'wordcount', 'imagetools', 'textpattern', 'noneditable',
+                'help', 'charmap', 'quickbars', 'emoticons'],
+            toolbar: 'bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist table twoColumn threeColumn | preview pagebreak removeformat print visualblocks',  // | rarely_used
+            quickbars_insert_toolbar: 'link image | numlist bullist table twoColumn threeColumn | hr pagebreak',
+            toolbar_groups: {
+                rarely_used: {
+                    icon: 'more-drawer',
+                    tooltip: 'Rarely Used',
+                    items: 'ltr rtl | charmap emoticons | superscript subscript | nonbreaking anchor media | undo redo | '
+                }
+            },
+            font_formats: 'Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Cambria=cambria,serif; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats',
+            pagebreak_split_block: true,
+            pagebreak_separator: '<span class="page-break-here"><!-- my page break --></span>',
+            nonbreaking_force_tab: true,
+            insertdatetime_formats: ['%d-%m-%Y %H:%M:%S', '%d-%m-%Y', '%H:%M:%S', '%I:%M:%S %p'],
+            content_css: $textArea.attr('data-css-url-render'),
+            content_style: `
+                body { font-family: Cambria,sans-serif; font-size: 12pt; }
+                @import url('/static/assets/fonts/cambria/Cambria.ttf');
+                @media print {
+                    .mce-visual-caret {
+                        display: none;
+                    }
+                }
+            `
+        });
+    }
+}
--- apps/kms/incomingdocument/static/assets/js/kms/incoming-doc/create.js
+++ apps/kms/incomingdocument/static/assets/js/kms/incoming-doc/create.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
     new $x.cls.file($('#attachment')).init({'name': 'attachment'});
     WFRTControl.setWFInitialData('kmsincomingdocument');
-})
+
+    IncomingDocLoadDataHandle.initPage();
+    const remark = new loadEditor();
+    remark.init();
+
+     $('#frm_create_incoming_document').validate({
+        submitHandler: function (form) {
+            let form_data = IncomingDocLoadDataHandle.combineData(form, 'create');
+            if (form_data) {
+                WFRTControl.callWFSubmitForm(form_data);
+            }
+        }
+    })
+});

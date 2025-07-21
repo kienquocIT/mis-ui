fbe2c2b49dbff6aa2c194df3e79c2d04cddcb5b3
phuclh <phuclh@mtsolution.com.vn>
2025-07-21 17:36:20 +0700

    [feature/ms-274] update shift assignment

--- apps/hrm/attendance/static/assets/hrm/attendance/js/shiftassignment/shift_assignment_common.js
+++ apps/hrm/attendance/static/assets/hrm/attendance/js/shiftassignment/shift_assignment_common.js
@@ -10,16 +10,7 @@ class ShiftAssignHandle {
     static $transEle = $('#app-trans-factory');
     static $urlEle = $('#app-url-factory');
 
-    static UpData = {};
-    static DateListCallable = []
-
-    static checkMargin(data){
-        let totalClass = ''
-        if (data.d_from !== data.d_to)
-            if (!data.time_from) totalClass += 'half_left '
-            if (data.time_to) totalClass += 'half_right '
-        return totalClass
-    }
+    static calendarInfo = null;
 
     static prepareData(data) {
         let afterPrepare = {
@@ -116,16 +107,57 @@ class ShiftAssignHandle {
             afterPrepare.des = data.meeting_content
         }
         return afterPrepare
-    }
+    };
 
     static addMinutesToDatetime(dateTimeString, minutesToAdd) {
         const dateObj = new Date(dateTimeString);
         let mili = dateObj.getTime() + minutesToAdd * 60 * 1000
         return moment(mili).format('YYYY-MM-DD HH:mm:ss')
+    };
+
+    static loadDtbEmployee(idTbl, groupID) {
+        $('#' + idTbl).not('.dataTable').DataTableDefault({
+            useDataServer: true,
+            ajax: {
+                url: ShiftAssignHandle.$urlEle.attr('data-dd-employee'),
+                type: 'GET',
+                data: {'group_id': groupID},
+                dataSrc: function (resp) {
+                    let data = $.fn.switcherResp(resp);
+                    if (data) return resp.data['employee_list'] ? resp.data['employee_list'] : [];
+                    return [];
+                },
+            },
+            pageLength: 5,
+            info: false,
+            columns: [
+                {
+                    title: ShiftAssignHandle.$transEle.attr('data-employee'),
+                    width: '70%',
+                    render: (data, type, row) => {
+                        return `<div class="form-check form-check-lg d-flex align-items-center">
+                                            <input type="checkbox" name="checkbox_employee" class="form-check-input checkbox-employee">
+                                            <label class="form-check-label">${row?.['full_name']}</label>
+                                        </div>`;
+                    }
+                },
+                {
+                    title: ShiftAssignHandle.$transEle.attr('data-code'),
+                    width: '30%',
+                    render: (data, type, row) => {
+                        return `<span class="form-check-label">${row?.['code']}</span>`;
+                    }
+                },
+            ],
+            drawCallback: function () {
+                // add css to Dtb
+                ShiftAssignHandle.loadDtbHideHeader(idTbl);
+            },
+        });
     }
 
     static init(calendar) {
-        calendar.render()
+        calendar.render();
 
         // click show detail of calendar
         $(document).on("click", ".calendarapp-wrap .fc-daygrid-event", function () {
@@ -177,6 +209,30 @@ class ShiftAssignHandle {
             else window.DateListCallable.push(DStart.format('YYYY-MM'))
         });
 
+    };
+
+    static generateEvents(startDate, endDate) {
+        let events = [];
+        let mStart = moment(startDate);
+        let mEnd = moment(endDate);
+
+        for (let m = mStart.clone(); m.isBefore(mEnd); m.add(1, 'days')) {
+            events.push({
+                title: "Ca HC",
+                start: m.format('YYYY-MM-DD'),
+                extendedProps: {
+                    toHtml: 'convert'
+                }
+            });
+        }
+
+        return events;
+    };
+
+    static loadShiftEmployee(calendar, info) {
+        let events = ShiftAssignHandle.generateEvents(info.start, info.end);
+        calendar.removeAllEvents();
+        calendar.addEventSource(events);
     }
 
     // DataTable
@@ -227,7 +283,30 @@ $(document).ready(function () {
         },
         eventClick: function (info) {
             targetEvent = info.event;
+        },
+        datesSet: function (info) {
+            // let events = ShiftAssignHandle.generateEvents(info.start, info.end);
+            // calendar.removeAllEvents();
+            // calendar.addEventSource(events);
+            ShiftAssignHandle.calendarInfo = info;
+        },
+        eventDidMount: function (info) {
+            let eventEle = info.el;
+            let eventFcMainEle = info.el.querySelector('.fc-event-main');
+            if (info.event.extendedProps.toHtml === 'convert') {
+                $(eventEle).css({
+                    'background-color': 'transparent',
+                    'border': 'none',
+                })
+                $(eventFcMainEle).css({
+                    'color': 'inherit',
+                    'float': 'right',
+                    'font-size': '10px',
+                    'margin-top': '25px',
+                })
+            }
         }
+
     });
 
     setTimeout(function () {
@@ -264,7 +343,7 @@ $(document).ready(function () {
             },
         },
         pageLength:50,
-        scrollY: '70vh',
+        scrollY: '76vh',
         searching: false,
         info: false,
         columns: [
@@ -308,49 +387,27 @@ $(document).ready(function () {
             iconEle.removeClass('text-secondary').addClass('text-dark');
 
             if (!trEle.next().hasClass('child-workflow-list')) {
-                let dtlSub = `<table id="${idTbl}" class="table nowrap w-100"><thead></thead><tbody></tbody></table>`
+                let dtlSub = `<table id="${idTbl}" class="table table-child nowrap w-100"><thead></thead><tbody></tbody></table>`
                 $(this).closest('tr').after(
                     `<tr class="child-workflow-list"><td colspan="4"><div class="child-workflow-group hidden-simple">${dtlSub}</div></td></tr>`
                 );
-                $('#' + idTbl).DataTableDefault({
-                    useDataServer: true,
-                    ajax: {
-                        url: ShiftAssignHandle.$urlEle.attr('data-dd-employee'),
-                        type: 'GET',
-                        data: {'group_id': $(this).attr('data-group-id')},
-                        dataSrc: function (resp) {
-                            let data = $.fn.switcherResp(resp);
-                            if (data) return resp.data['employee_list'] ? resp.data['employee_list'] : [];
-                            return [];
-                        },
-                    },
-                    pageLength: 5,
-                    info: false,
-                    columns: [
-                        {
-                            title: ShiftAssignHandle.$transEle.attr('data-employee'),
-                            render: (data, type, row) => {
-                                return `<div class="form-check form-check-lg d-flex align-items-center">
-                                            <input type="checkbox" name="checkbox_employee" class="form-check-input">
-                                            <label class="form-check-label">${row?.['full_name']}</label>
-                                        </div>`;
-                            }
-                        },
-                        {
-                            title: ShiftAssignHandle.$transEle.attr('data-code'),
-                            render: (data, type, row) => {
-                                return `<span class="form-check-label">${row?.['code']}</span>`;
-                            }
-                        },
-                    ],
-                    drawCallback: function () {
-                        // add css to Dtb
-                        ShiftAssignHandle.loadDtbHideHeader(idTbl);
-                    },
-                });
             }
+            if (trEle.next().hasClass('child-workflow-list')) {
+                let $tableChildEle = trEle.next().find('.table-child');
+                if ($tableChildEle.length > 0) {
+                    idTbl = $tableChildEle[0].id;
+                    if ($.fn.dataTable.isDataTable($tableChildEle)) {
+                        $tableChildEle.DataTable().destroy();
+                    }
+                }
+            }
+            ShiftAssignHandle.loadDtbEmployee(idTbl, $(this).attr('data-group-id'));
             trEle.next().removeClass('hidden').find('.child-workflow-group').slideToggle();
         }
+    });
+
+    ShiftAssignHandle.$table.on('click', '.checkbox-employee', function () {
+        ShiftAssignHandle.loadShiftEmployee(calendar, ShiftAssignHandle.calendarInfo);
     });
 
 });

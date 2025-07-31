fca76e87af5192fa4ff6b7936a91794079bf1d6b
namviet <vietnn@mtsolution.com>
2025-07-31 15:54:15 +0700

    [feature/ms-008] hide group order menu

--- apps/shared/menus.py
+++ apps/shared/menus.py
@@ -378,10 +378,10 @@ class MenusCRM:
         name='Sale order', code='menu_sale_order_list', view_name='SaleOrderList',
         icon='<i class="fas fa-shopping-cart"></i>',
     )
-    GROUP_ORDER = MenuCommon(
-        name='Group order', code='menu_group_order_list', view_name='GroupOrderList',
-        icon='<i class="fas fa-file-invoice"></i>',
-    )
+    # GROUP_ORDER = MenuCommon(
+    #     name='Group order', code='menu_group_order_list', view_name='GroupOrderList',
+    #     icon='<i class="fas fa-file-invoice"></i>',
+    # )
     LEASE_ORDER = MenuCommon(
         name='Lease order', code='menu_lease_order_list', view_name='LeaseOrderList',
         icon='<i class="fas fa-handshake"></i>',
@@ -1018,7 +1018,7 @@ class SpaceItem:
                 MenusCRM.CONSULTING,
                 MenusCRM.BIDDING,
                 MenusCRM.SALE_ORDER,
-                MenusCRM.GROUP_ORDER,
+                # MenusCRM.GROUP_ORDER,
                 MenusCRM.AR_INVOICE,
                 MenusCRM.LEASE_ORDER,
                 MenusCRM.LEASE_ASSET_LIST,

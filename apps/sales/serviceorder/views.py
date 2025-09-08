8b8cfd1ab5dc10d2714406d2d8b27532ba6c2c43
phuclh <phuclh@mtsolution.com.vn>
2025-09-08 09:34:50 +0700

    [feature/service-order] them task vao service_order detail

--- apps/sales/serviceorder/views.py
+++ apps/sales/serviceorder/views.py
@@ -77,7 +77,20 @@ class ServiceOrderDetail(View):
         breadcrumb='SERVICE_ORDER_DETAIL_PAGE',
     )
     def get(self, request, pk, *args, **kwargs):
-        return {'pk': pk}, status.HTTP_200_OK
+        employee_current = {}
+        if request.user and not isinstance(request.user, AnonymousUser):
+            employee_current = getattr(request.user, 'employee_current_data', {})
+        resp = ServerAPI(user=request.user, url=ApiURL.OPPORTUNITY_TASK_CONFIG).get()
+        task_config = {}
+        if resp.state:
+            task_config = resp.result
+        return {
+                   'pk': pk,
+
+                   'employee_current': employee_current,
+                   'task_config': task_config,
+                   'employee_info': request.user.employee_current_data,
+               }, status.HTTP_200_OK
 
 
 class ServiceOrderDetailAPI(APIView):

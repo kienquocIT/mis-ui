66d3629f6518a512a66e82ec5955cfe9fa7cf3db
furykid <candvg@mtsolution.com.vn>
2025-10-03 17:54:18 +0700

    [feature/service-order]: complete to load work order and coressponding checkbox to work order

--- apps/sales/serviceorder/static/assets/sales/serviceorder/js/service_order_list.js
+++ apps/sales/serviceorder/static/assets/sales/serviceorder/js/service_order_list.js
@@ -1,4 +1,6 @@
 class loadServiceOrderInfo {
+    static selectedWorkOrderIds = [];
+
     static loadServiceOrderList() {
         if (!$.fn.DataTable.isDataTable('#table-service-order')) {
             const $tb = $('#table-service-order');
@@ -82,39 +84,135 @@ class loadServiceOrderInfo {
                         }
                     }
                 ],
-            })
+            });
         }
     }
 
-    static loadDeliveryWorkOrderList() {
-        if (!$.fn.DataTable.isDataTable('#delivery_work_order_list')) {
-            const $tb = ('#delivery_work_order_list');
-            $tb.DataTableDefault({
-                useDataServer: true,
-                rowIdx: true,
-                scrollX: true,
-                scrollY: '50vh',
-                scrollCollapse: true,
-                reloadCurrency: true,
-                ajax: {
-                    url: $tb.attr('data-url'),
-                    type: 'GET',
-                    dataSrc: 'data.svo_work_order_detail'
+    static loadDeliveryWorkOrderList(service_order_id) {
+        const $tb = $('#delivery_work_order_list');
+        $tb.DataTable().clear().destroy();
+        $tb.DataTableDefault({
+            useDataServer: true,
+            rowIdx: true,
+            scrollX: true,
+            scrollY: '50vh',
+            scrollCollapse: true,
+            reloadCurrency: true,
+            ajax: {
+                url: $tb.attr('data-url') + `?service_order_id=${service_order_id}`,
+                type: 'GET',
+                dataSrc: 'data.svo_work_order_detail'
+            },
+            columns: [
+                {
+                    className: "w-10",
+                    render: () => {
+                        return "";
+                    }
                 },
-                columns: [
-                    {
-                        className: "w-5",
-                        render: () => {
-                            return ""
+                {
+                    className: "w-10",
+                    render: (data, type, row) => {
+                        let checked = loadServiceOrderInfo.selectedWorkOrderIds.includes(row?.id) ? 'checked' : '';
+                        return `<div class="form-check form-check-lg">
+                                    <input type="checkbox" id="checkbox_${row?.id}" class="form-check-input row-checkbox"
+                                            value="${row?.id}" ${checked}>
+                                </div>`;
+                    }
+                },
+                {
+                    className: "w-40",
+                    render: (data, type, row) => {
+                        return row?.['title'] || '--';
+                    }
+                },
+                {
+                    className: "w-20",
+                    render: (data, type, row) => {
+                        let startDate = DateTimeControl.formatDateType('YYYY-MM-DD','DD/MM/YYYY', row?.['start_date'] || '--');
+                        return startDate;
+                    }
+                },
+                {
+                    className: "w-20",
+                    render: (data, type, row) => {
+                        let endDate = DateTimeControl.formatDateType('YYYY-MM-DD','DD/MM/YYYY', row?.['end_date'] || '--');
+                        return endDate;
+                    }
+                },
+            ],
+            rowCallback: function(row, data) {
+                const $checkbox = $(row).find('.row-checkbox');
+
+                $checkbox.off("change").on("change", function () {
+                    let work_order_id = parseInt($(this).val());
+                    if ($(this).is(":checked")) {
+                        if (!loadServiceOrderInfo.selectedWorkOrderIds.includes(work_order_id)) {
+                            loadServiceOrderInfo.selectedWorkOrderIds.push(work_order_id);
+                        } else {
+                            loadServiceOrderInfo.selectedWorkOrderIds = loadServiceOrderInfo.selectedWorkOrderIds.filter(x => x !== work_order_id);
                         }
-                    },
-                ]
-            })
-        }
+                    }
+                });
+            }
+        });
+    }
+
+    static loadTotalDeliveryProduct(service_order_id) {
+        const $tb = $('#total_delivery_product');
+        $tb.DataTable().clear().destroy();
+        $tb.DataTableDefault({
+            useDataServer: true,
+            rowIdx: true,
+            scrollX: true,
+            scrollY: '50vh',
+            scrollCollapse: true,
+            reloadCurrency: true,
+            ajax: {
+                url: $tb.attr('data-url') + `?service_order_id=${service_order_id}`,
+                type: 'GET',
+                dataSrc: 'data.svo_work_order_detail'
+            },
+            columns: [
+                {
+                    className: "w-10",
+                    render: () => {
+                        return "";
+                    }
+                },
+                {
+                    className: "w-10",
+                    render: (data, type, row) => {
+                        let checked = loadServiceOrderInfo.selectedWorkOrderIds.includes(row?.id) ? 'checked' : '';
+                        return `<div class="form-check form-check-lg">
+                                    <input type="checkbox" id="checkbox_${row?.id}" class="form-check-input row-checkbox"
+                                            value="${row?.id}" ${checked}>
+                                </div>`;
+                    }
+                },
+                {
+                    className: "w-50",
+                    render: (data, type, row) => {
+                        return row?.['title'] || '--';
+                    }
+                },
+                {
+                    className: "w-30",
+                    render: (data, type, row) => {
+                        let startDate = DateTimeControl.formatDateType('YYYY-MM-DD','DD/MM/YYYY', row?.['start_date'] || '--');
+                        return startDate;
+                    }
+                },
+            ],
+        });
     }
 }
 
 $('document').ready(function () {
     loadServiceOrderInfo.loadServiceOrderList();
+    $(document).on("click", '.open-delivery-modal', function () {
+        let service_order_row_id = $(this).attr('data-id');
+        loadServiceOrderInfo.loadDeliveryWorkOrderList(service_order_row_id);
+        loadServiceOrderInfo.loadTotalDeliveryProduct(service_order_row_id);
+    })
 });
-

d6e36d407412fe105c4e3f865e7dc0d3c8de285f
namviet <vietnn@mtsolution.com>
2025-10-14 14:24:43 +0700

    [feature/ms-008] copy quotation service quotation

--- apps/sales/servicequotation/static/assets/sales/servicequotation/js/service_quotation_create.js
+++ apps/sales/servicequotation/static/assets/sales/servicequotation/js/service_quotation_create.js
@@ -168,4 +168,245 @@ $(document).ready(function () {
     handleModalPaymentDetailEvent()
 
     setUpFormSubmit($('#form-create-service-quotation'))
+
+
+    class ServiceQuotationCopyCanvas {
+        constructor() {
+            this.$canvas = $('#offcanvas-copy-service-quotation')
+            this.$table = $('#datatable-copy-service-quotation')
+            this.$selectBtn = $('#btn-select-quotation-copy')
+            this.currentQuotId = null
+        }
+
+        static loadDetailOpp(data){
+            new $x.cls.bastionField({
+            data_opp: data?.['opportunity']?.['id'] ? [
+                {
+                    "id": data?.['opportunity']?.['id'],
+                    "title": data?.['opportunity']?.['title'] || '',
+                    "code": data?.['opportunity']?.['code'] || '',
+                    "selected": true,
+                }
+            ] : [],
+            data_inherit: data?.['employee_inherit']?.['id'] ? [
+                {
+                    "id": data?.['employee_inherit']?.['id'],
+                    "full_name": data?.['employee_inherit']?.['full_name'] || '',
+                    "code": data?.['employee_inherit']?.['code'] || '',
+                    "selected": true,
+                }
+            ] : [],
+             // "oppFlagData": {"disabled": !isUpdate},
+             // "inheritFlagData": {"disabled": true},
+            }).init()
+        }
+
+        static loadCustomerList(data) {
+            ServiceOrder.pageElement.commonData.$customer.initSelect2({
+                ajax: {
+                    url: ServiceOrder.pageElement.commonData.$customer.attr('data-url'),
+                    method: 'GET'
+                },
+                data: (data ? data : null),
+                keyResp: 'account_dd_list',
+                keyId: 'id',
+                keyText: 'name'
+            })
+        }
+
+        static formatShipmentDetailData(shipmentData) {
+            const result = [];
+            if (shipmentData.length > 0) {
+                const containers = shipmentData.filter(item => item?.is_container === true);
+                const packages = shipmentData.filter(item => item?.is_container === false);
+
+                containers.sort((a, b) => a?.order - b?.order);   // sort container by order
+                containers.forEach(container => {
+                    result.push(container);
+
+                    const containerPackages = packages.filter(pkg =>
+                        pkg['packageContainerRef'] === container?.containerRefNumber
+                    );
+
+                    containerPackages.sort((a, b) => a?.order - b?.order);  // sort package by order
+                    result.push(...containerPackages);
+                });
+                return result;
+            } else {
+                return [];
+            }
+        }
+
+        getSelectedQuotationId(){
+            const self= this
+            return self.currentQuotId
+        }
+
+        updateSelectedQuotationId(quotationId){
+            const self= this
+            self.currentQuotId = quotationId
+        }
+
+        init(){
+            const self = this
+            self.initDataTable()
+            self.initEvents()
+        }
+
+        initDataTable(){
+            const self= this
+            self.$table.DataTableDefault({
+                useDataServer: true,
+                rowIdx: false,
+                ajax: {
+                url: self.$canvas.attr('data-quotation-list-url'),
+                    type: 'GET',
+                    dataSrc: function (resp) {
+                        let data = $.fn.switcherResp(resp);
+                        if (data) {
+                            return resp.data['service_quotation_list'] ? resp.data['service_quotation_list'] : [];
+                        }
+                        return [];
+                    },
+                },
+                columns: [
+                    {
+                        className: 'w-25',
+                        render: (data, type, row) => {
+                            const quotationId = row['id']
+                            return `<div class="form-check form-check-lg d-flex align-items-center">
+                                        <input type="radio" name="row-check" class="form-check-input table-row-check" id="${quotationId}" data-id="${row?.['id']}">
+                                        <label class="form-check-label table-row-title" for="${quotationId}">${row?.['title']}</label>
+                                    </div>`;
+                        }
+                    },
+                    {
+                        className: 'w-25',
+                        render: (data, type, row) => {
+                            return `<span>${row?.['code'] ? row?.['code'] : '__'}</span>`;
+                        }
+                    },
+                    {
+                        className: 'w-25',
+                        render: (data, type, row) => {
+                            if (row?.['opportunity']?.['title'] && row?.['opportunity']?.['code']) {
+                                return `<span>${row?.['opportunity']?.['title']}</span>`;
+                            }
+                            return `__`;
+                        }
+                    },
+                    {
+                        className: 'w-25',
+                        render: (data, type, row) => {
+                            if (row?.['customer_data']?.['name']) {
+                                return `<p>${row?.['customer_data']?.['name']}</p>`;
+                            }
+                            return `__`;
+                        }
+                    }
+                ]
+            })
+        }
+
+        initEvents(){
+            const self= this
+            self.$selectBtn.on('click', (e) => {
+                const templateDetailUrl = self.$canvas.attr('data-quotation-detail')
+                const detailUrl = templateDetailUrl.format_url_with_uuid(self.getSelectedQuotationId())
+                $.fn.callAjax2({
+                    url: detailUrl,
+                    method: 'GET'
+                }).then(
+                    (resp) => {
+                        const data = $.fn.switcherResp(resp);
+                        $x.fn.renderCodeBreadcrumb(data);
+                        $.fn.compareStatusShowPageAction(data);
+                        new $x.cls.file($('#attachment')).init({
+                            name: 'attachment',
+                            enable_edit: true,
+                            enable_download: true,
+                            data: data?.['attachment'],
+                        })
+
+                        ServiceQuotationCopyCanvas.loadDetailOpp(data)
+
+                        const startDate = data.start_date ? DateTimeControl.formatDateType(
+                            "YYYY-MM-DD",
+                            "DD/MM/YYYY",
+                            data.start_date
+                        ) : ''
+
+                        const endDate = data.end_date ? DateTimeControl.formatDateType(
+                            "YYYY-MM-DD",
+                            "DD/MM/YYYY",
+                            data.end_date
+                        ) : ''
+
+                        // basic information fields
+                        ServiceOrder.pageElement.commonData.$titleEle.val(data?.title)
+                        ServiceQuotationCopyCanvas.loadCustomerList(data?.customer_data)
+                        ServiceOrder.pageElement.commonData.$startDate.val(startDate)
+                        ServiceOrder.pageElement.commonData.$endDate.val(endDate)
+                        console.log(data?.exchange_rate_data)
+                        ServiceOrder.loadExchangeRateData(data?.exchange_rate_data)
+
+                        // shipment
+                        let shipmentDataFormatted = ServiceQuotationCopyCanvas.formatShipmentDetailData(data?.shipment || [])
+                        TabShipmentFunction.initShipmentDataTable(shipmentDataFormatted, false)
+                        TabShipmentFunction.pushToShipmentData(shipmentDataFormatted)
+
+                        //service detail
+                        ServiceOrder.initServiceDetailDataTable(data.service_detail_data)
+                        ServiceOrder.loadServiceDetailRelatedData(data.service_detail_data)
+                        ServiceOrder.loadServiceDetailSummaryValue()
+
+                        //work order
+                        let workOrderData = data.work_order_data
+                        workOrderData.forEach(workOrder => {
+                            workOrder.start_date = DateTimeControl.formatDateType(
+                                "YYYY-MM-DD",
+                                "DD/MM/YYYY",
+                                workOrder.start_date
+                            )
+                            workOrder.end_date = DateTimeControl.formatDateType(
+                                "YYYY-MM-DD",
+                                "DD/MM/YYYY",
+                                workOrder.end_date
+                            )
+                        })
+                        ServiceOrder.initQuotationWorkOrderDataTable(workOrderData)
+                        ServiceOrder.loadWorkOrderRelatedData(workOrderData)
+
+                        //payment
+                        let paymentData = data.payment_data
+                        paymentData.forEach(payment => {
+                            payment.due_date = DateTimeControl.formatDateType(
+                                "YYYY-MM-DD",
+                                "DD/MM/YYYY",
+                                payment.due_date
+                            )
+                        })
+                        ServiceOrder.initPaymentDataTable(paymentData)
+                        ServiceOrder.loadPaymentRelatedData(paymentData)
+
+                        //expense
+                        tabExpenseElements.$preTaxAmount.attr('value', data?.expense_pretax_value || 0)
+                        tabExpenseElements.$taxEle.attr('value', data?.expense_tax_value || 0)
+                        tabExpenseElements.$totalValueEle.attr('value', data?.expense_total_value || 0)
+                        TabExpenseFunction.initExpenseTable(data?.expense || [], false)
+
+                        $.fn.initMaskMoney2()
+                        WFRTControl.setWFRuntimeID(data?.['workflow_runtime_id'])
+                    })
+            })
+
+            self.$table.on('click', 'input[type="radio"]', (e)=>{
+                self.updateSelectedQuotationId($(e.currentTarget).attr('data-id'))
+                console.log($(e.currentTarget).attr('data-id'))
+            })
+        }
+    }
+
+    const serviceQuotationCopyCanvas = new ServiceQuotationCopyCanvas()
+    serviceQuotationCopyCanvas.init()
 })

8e98d32d00fa783b7ad736a28a99dcfa481e45ed
furykid <candvg@mtsolution.com.vn>
2025-10-15 14:51:58 +0700

    [feature/payroll_devcan]: complete to update and load detail for Payroll config function

--- apps/hrm/payroll/static/assets/hrm/payroll/js/payrollconfig/payroll_config.js
+++ apps/hrm/payroll/static/assets/hrm/payroll/js/payrollconfig/payroll_config.js
@@ -1,10 +1,24 @@
 $(document).ready(function () {
-    UsualLoadPageFunction.LoadDate({element: pageElements.$effectiveDate, empty: true});
+    UsualLoadPageFunction.LoadDate({element: payrollConfigElements.$effectiveDate, empty: true});
     PayrollConfigDataHandler.initTaxBracketTable();
-    $('#frm_payroll_config').submit(function (e) {
-        e.preventDefault()
+    PayrollConfigDataHandler.loadDetailPayrollConfig();
+
+    payrollConfigElements.$frmPayrollConfig.on('submit', function (e) {
+        e.preventDefault();
         let _form = new SetupFormSubmit($(this));
+        try {
+            PayrollConfigDataHandler.combinePayrollConfigData(_form);  // combine data
+        } catch (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Invalid input',
+                text: error.message
+            });
+            return;
+        }
+
         let csr = $("[name=csrfmiddlewaretoken]").val();
+
         $.fn.callAjax(_form.dataUrl, _form.dataMethod, _form.dataForm, csr)
             .then(
                 (resp) => {
@@ -12,11 +26,16 @@ $(document).ready(function () {
                     if (data) {
                         $.fn.notifyB({description: data.message}, 'success');
                         $.fn.redirectUrl($(this).attr('data-url-redirect'), 1000);
+                        PayrollConfigDataHandler.loadDetailPayrollConfig();
                     }
                 },
                 (errs) => {
                     console.log(errs);
                 }
-            )
+            );
+    });
+
+    $('#btn_update_payroll_config').on('click', function () {
+        payrollConfigElements.$frmPayrollConfig.submit();
     });
 });

94c454361850ec7583559192d17154c755ca06a0
namviet <vietnn@mtsolution.com>
2025-11-04 12:32:14 +0700

    [feature/service-order-versioning] fix load customer service order update page

--- apps/sales/serviceorder/static/assets/sales/serviceorder/js/service_order_update.js
+++ apps/sales/serviceorder/static/assets/sales/serviceorder/js/service_order_update.js
@@ -1,10 +1,10 @@
 $(document).ready(async function() {
     WFRTControl.setWFInitialData('serviceorder');
     WindowControl.showLoading()
+    ServiceOrderPageHandler.initializeComponents(false)
     await ServiceOrderPageHandler.initializeCommonData();
     await ServiceOrderPageHandler.loadServiceOrderData($('#form-update-service-order').attr('data-url'), false);
     WindowControl.hideLoading()
-    ServiceOrderPageHandler.initializeComponents(false)
     ServiceOrderPageHandler.registerCommonEventHandlers()
     ServiceOrderPageHandler.setupFormSubmit('#form-update-service-order')
     ServiceOrderPageHandler.setupIndicatorCanvas('#form-update-service-order')

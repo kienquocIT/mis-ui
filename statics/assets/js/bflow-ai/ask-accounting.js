f29bb2286b5711d862bc8b7d4c4223895049b850
ndh34 <nguyenduongghai07@gmail.com>
2025-12-02 17:30:19 +0700

    [feature/hotfix-haind]: hot fix quantity TU TT HU

--- statics/assets/js/bflow-ai/ask-accounting.js
+++ statics/assets/js/bflow-ai/ask-accounting.js
@@ -9,17 +9,48 @@ $(document).ready(function () {
     const $typingIndicator = $('#typingIndicator');
     const $bai_script_url = $('#bai-script-url');
 
-    let session_key = ""
+    class Decorate {
+        // Show typing indicator with enhanced animation
+        static showTypingIndicator() {
+            // Add typing indicator to chat body
+            $chatBody.append($typingIndicator);
+
+            // Show with animation
+            setTimeout(() => {
+                $typingIndicator.addClass('active');
+                // Smooth scroll to bottom
+                $chatBody.animate({scrollTop: $chatBody[0].scrollHeight}, 300);
+            }, 100);
+        }
+        // Hide typing indicator with animation
+        static hideTypingIndicator() {
+            $typingIndicator.removeClass('active');
+            setTimeout(() => {
+                $typingIndicator.detach();
+            }, 300);
+        }
+        // Add message to chat
+        static addMessage(content, sender, is_hello = false) {
+            const $messageDiv = $('<div>').addClass(`ai-message ${sender}`);
+            const $contentDiv = $('<div>').addClass(`ai-message-content ${is_hello ? 'is_hello' : ''}`).html(content);
+
+            $messageDiv.append($contentDiv);
+            $chatBody.append($messageDiv);
+
+            // Scroll to bottom
+            $chatBody.animate({scrollTop: $chatBody[0].scrollHeight}, 300);
+        }
+    }
 
     // Toggle chat visibility
     $chatButton.on('click', function () {
         $chatContainer.toggleClass('active');
         if ($chatContainer.hasClass('active')) {
             if ($chatBody.find('.is_hello').length === 0) {
-                showTypingIndicator();
+                Decorate.showTypingIndicator();
                 setTimeout(() => {
-                    hideTypingIndicator();
-                    addMessage('Xin chào👋! Tôi là trợ lí Bflow AI. Tôi có thể giúp gì cho bạn?', 'ai', true);
+                    Decorate.hideTypingIndicator();
+                    Decorate.addMessage('Xin chào👋! Tôi là trợ lí Bflow AI. Tôi có thể giúp gì cho bạn?', 'ai', true);
                 }, 1000);
             }
             $chatInput.focus();
@@ -30,96 +61,6 @@ $(document).ready(function () {
     $chatClose.on('click', function () {
         $chatContainer.removeClass('active');
     });
-
-    // Send message
-    function sendMessage(data_url) {
-        const message = $chatInput.val().trim();
-        if (message) {
-            // Add user message
-            addMessage(message, 'user');
-            $chatInput.val('');
-            $chatSend.prop('disabled', true);
-
-            // Show typing indicator
-            showTypingIndicator();
-
-            // Hard response or Call AI API here
-            hideTypingIndicator();
-            let aiResponse = '';
-            $.ajax({
-                url: 'http://127.0.0.1:8002/bflow-ai/accounting-api/',
-                method: 'POST',
-                data: JSON.stringify({context: message}),
-                contentType: "application/json",
-                processData: false,
-                headers: {"X-CSRFToken": $('input[name="csrfmiddlewaretoken"]').val()},
-                success: function (res) {
-                    if (res.data) {
-                        console.log(res.data)
-                        let accountData = res.data
-                        let aiResponse = `
-                        <div class="account-info">
-                            <h4 class="fw-bold px-1 py-2">${accountData["VietnameseName"]} (${accountData["EnglishName"]})</h4>
-                            <p><strong class="fw-bold">Số tài khoản:</strong> ${accountData["AccountNumber"]}</p>
-                            <p><strong class="fw-bold">Phương pháp kế toán:</strong> ${accountData["AccountingMethod"]}</p>
-                            <p><strong class="fw-bold">Tài khoản đối ứng:</strong> ${accountData["CorrespondingAccounts"]}</p>
-                            <p><strong class="fw-bold">Ý nghĩa:</strong> ${accountData["Meaning"]}</p>
-                            <p><strong class="fw-bold">Cách sử dụng:</strong> ${accountData["Usage"]}</p>
-                        </div>
-                        `;
-                        addMessage(aiResponse, 'ai');
-                        $chatSend.prop('disabled', false);
-                    } else {
-                        console.log(res.message)
-                        let aiResponse = res.message
-                        addMessage(aiResponse, 'ai');
-                        $chatSend.prop('disabled', false);
-                    }
-
-                },
-                error: function (error) {
-                    console.log(error)
-                    aiResponse = error
-                    addMessage(error, 'ai');
-                    $chatSend.prop('disabled', false);
-                }
-            })
-
-        }
-    }
-
-    // Add message to chat
-    function addMessage(content, sender, is_hello = false) {
-        const $messageDiv = $('<div>').addClass(`ai-message ${sender}`);
-        const $contentDiv = $('<div>').addClass(`ai-message-content ${is_hello ? 'is_hello' : ''}`).html(content);
-
-        $messageDiv.append($contentDiv);
-        $chatBody.append($messageDiv);
-
-        // Scroll to bottom
-        $chatBody.animate({scrollTop: $chatBody[0].scrollHeight}, 300);
-    }
-
-    // Show typing indicator with enhanced animation
-    function showTypingIndicator() {
-        // Add typing indicator to chat body
-        $chatBody.append($typingIndicator);
-
-        // Show with animation
-        setTimeout(() => {
-            $typingIndicator.addClass('active');
-            // Smooth scroll to bottom
-            $chatBody.animate({scrollTop: $chatBody[0].scrollHeight}, 300);
-        }, 100);
-    }
-
-    // Hide typing indicator with animation
-    function hideTypingIndicator() {
-        $typingIndicator.removeClass('active');
-        setTimeout(() => {
-            $typingIndicator.detach();
-        }, 300);
-    }
 
     // Send message on button click
     $chatSend.on('click', function () {
@@ -138,4 +79,46 @@ $(document).ready(function () {
             $chatContainer.removeClass('active');
         }
     });
+
+    async function askAI(userQuestion) {
+        const url = `http://127.0.0.1:8010/api/v1/ask?question=${userQuestion}`
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {}
+            });
+            if (!response.ok) {
+                throw new Error(`Lỗi Server: ${response.status}`);
+            }
+            Decorate.hideTypingIndicator();
+            const data = await response.json();
+            if (data) {
+                Decorate.addMessage(data, 'ai');
+                $chatSend.prop('disabled', false);
+            } else {
+
+                let aiResponse = data.message
+                Decorate.addMessage(aiResponse, 'ai');
+                $chatSend.prop('disabled', false);
+            }
+        } catch (error) {
+            console.error("Lỗi kết nối:", error);
+        }
+    }
+
+    // Send message
+    async function sendMessage() {
+        const message = $chatInput.val().trim();
+        if (message) {
+            // Add user message
+            Decorate.addMessage(message, 'user');
+            $chatInput.val('');
+            $chatSend.prop('disabled', true);
+
+            // Show typing indicator
+            Decorate.showTypingIndicator();
+
+            await askAI(message);
+        }
+    }
 });
